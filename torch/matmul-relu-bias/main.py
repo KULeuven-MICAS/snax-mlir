9dd9fd0abb7d4fd39af8f10b30704216ad4ee5fc
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2023-09-14 12:08:52 +0200

    change example back to floating point

--- torch/matmul-relu-bias/main.py
+++ torch/matmul-relu-bias/main.py
@@ -2,9 +2,13 @@ import torch
 from stardew.torch import torch_compiler
 
 
-x = torch.randint(-10, 10, (256, 128), dtype=torch.int32)
-w = torch.randint(-10, 10, (128,  10), dtype=torch.int32)
-b = torch.randint(-10, 10, ( 10,    ), dtype=torch.int32)
+# These should ideally also be converted to integer datatypes,
+# but torch-mlir does not support non floating-point types for
+# the RELU activation function
+
+x = torch.randint(-10, 10, (256, 128), dtype=torch.float32)
+w = torch.randint(-10, 10, (128,  10), dtype=torch.float32)
+b = torch.randint(-10, 10, ( 10,    ), dtype=torch.float32)
 
 @torch_compiler(example=[x, w, b])
 def foo(x : torch.Tensor, w : torch.Tensor, b: torch.Tensor):
