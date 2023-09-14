7180022563b338b356acc992536531ad63f2f2f1
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2023-09-14 15:00:00 +0200

    tests restructured

--- torch/matmul-relu-bias/main.py
+++ torch/matmul-relu-bias/main.py
@@ -2,14 +2,22 @@ import torch
 from stardew.torch import torch_compiler
 
 
-# These should ideally also be converted to integer datatypes,
-# but torch-mlir does not support non floating-point types for
-# the RELU activation function
+def main():
+    # These should ideally also be converted to integer datatypes,
+    # but torch-mlir does not support non floating-point types for
+    # the RELU activation function
 
-x = torch.randint(-10, 10, (256, 128), dtype=torch.float32)
-w = torch.randint(-10, 10, (128,  10), dtype=torch.float32)
-b = torch.randint(-10, 10, ( 10,    ), dtype=torch.float32)
+    x = torch.randint(-10, 10, (256, 128), dtype=torch.float32)
+    w = torch.randint(-10, 10, (128,  10), dtype=torch.float32)
+    b = torch.randint(-10, 10, ( 10,    ), dtype=torch.float32)
 
-@torch_compiler(example=[x, w, b])
-def foo(x : torch.Tensor, w : torch.Tensor, b: torch.Tensor):
-    return torch.relu(x @ w + b)
+    @torch_compiler(example=[x, w, b])
+    def foo(x : torch.Tensor, w : torch.Tensor, b: torch.Tensor):
+        return torch.relu(x @ w + b)
+    
+
+def test_matmul():
+    main()
+
+if __name__ == "__main__":
+    main()
--- torch/mult/main.py
+++ torch/mult/main.py
@@ -1,11 +1,19 @@
 import torch
 from stardew.torch import torch_compiler
 
-vector_size = 128
 
-a = torch.randint(-10, 10, (vector_size,), dtype=torch.int32)
-b = torch.randint(-10, 10, (vector_size,), dtype=torch.int32)
+def main():
+    vector_size = 128
 
-@torch_compiler(example=[a,b])
-def mult(a, b):
-    return torch.mul(a,b)
+    a = torch.randint(-10, 10, (vector_size,), dtype=torch.int32)
+    b = torch.randint(-10, 10, (vector_size,), dtype=torch.int32)
+    
+    @torch_compiler(example=[a,b])
+    def foo(a, b):
+        return torch.mul(a,b)
+
+def test_mult():
+    main()
+
+if __name__ == "__main__":
+    main()
