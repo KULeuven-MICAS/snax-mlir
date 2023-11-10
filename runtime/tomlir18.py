ac7d9437803214ffe6c443cdad484814a6777cc7
jorendumoulin <47864363+jorendumoulin@users.noreply.github.com>
2023-11-10 15:25:44 +0100

    end-to-end workflow (#12)
    This commit implements an end-to-end flow from mlir to simulation on a snitch cluster. The steps taken look like this:
    
    Preprocess the input mlir to with generic printing and local scope
    input.mlir ->  input.preproc.mlir
    
    Translate MLIR 16 to MLIR18 with a python script
    input.preproc.mlir -> input.preproc18.mlir
    
    Apply the custom snax-opt compiler. This includes passes to lower the linalg generics to external function calls which drive the accelerator
    input.preproc18.mlir -> input.snax-opt18.mlir
    
    Translate back to MLIR 16
    input.snax-opt18.mlir -> input.snax-opt.mlir
    
    Now use the existing MLIR workflow to lower and translate the remaining MLIR to LLVM, compile and run

---
+++ runtime/tomlir18.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+""" Convert an MLIR18 file to MLIR16
+This program performs the following conversion steps:
+    *   change operand_segment_sizes to operandSegmentSizes
+    *   add double quotes to attribute names 
+"""
+
+import re
+import sys
+
+
+if __name__ == "__main__":
+    ir = sys.stdin.read()
+    # add quotes
+    ir = re.sub("indexing_maps", '"indexing_maps"', ir)
+    # add quotes
+    ir = re.sub("iterator_types", '"iterator_types"', ir)
+    # add quotes and change spelling
+    ir = re.sub("operand_segment_sizes", '"operandSegmentSizes"', ir)
+    print(ir)
