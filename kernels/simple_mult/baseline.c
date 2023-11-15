322aba501154603676522cf60ad0c45d30663807
Josse Van Delm <josse.vandelm@gmail.com>
2023-11-15 16:27:17 +0100

    Add memref wrappers

--- kernels/simple_mult/baseline.c
+++ kernels/simple_mult/baseline.c
@@ -4,9 +4,20 @@
 
 #include <stdint.h>
 
-void simple_mult(const int32_t *A, const int32_t *B, int32_t *D) {
+struct OneDMemrefI32 {
+  int32_t *data; // allocated pointer: Pointer to data buffer as allocated,
+                 // only used for deallocating the memref
+  int32_t *aligned_data; // aligned pointer: Pointer to properly aligned data
+                         // that memref indexes
+  uint32_t *offset;
+  uint32_t *shape[1];
+  uint32_t *stride[1];
+};
+
+void _mlir_ciface_simple_mult(struct OneDMemrefI32 *A, struct OneDMemrefI32 *B,
+                              struct OneDMemrefI32 *D) {
   const uint32_t n = N;
   for (uint32_t i = 0; i < n; ++i) {
-    D[i] = A[i] * B[i];
+    D->aligned_data[i] = A->aligned_data[i] * B->aligned_data[i];
   }
 }
--- kernels/simple_mult/main.c
+++ kernels/simple_mult/main.c
@@ -4,12 +4,27 @@
 
 #include <snrt.h>
 
-// Kernel provided via external definition
-void simple_mult(int32_t *a, int32_t *b, int32_t *d);
+struct OneDMemrefI32 {
+  int32_t *data; // allocated pointer: Pointer to data buffer as allocated,
+                 // only used for deallocating the memref
+  int32_t *aligned_data; // aligned pointer: Pointer to properly aligned data
+                         // that memref indexes
+  uint32_t *offset;
+  uint32_t *shape[1];
+  uint32_t *stride[1];
+};
 
-void snax_hwpe_mult(int32_t *a, int32_t *b, int32_t *d) {
+// Kernel provided via external definition
+void _mlir_ciface_simple_mult(struct OneDMemrefI32 *a, struct OneDMemrefI32 *b,
+                              struct OneDMemrefI32 *d);
+
+void _mlir_ciface_snax_hwpe_mult(struct OneDMemrefI32 *a,
+                                 struct OneDMemrefI32 *b,
+                                 struct OneDMemrefI32 *d) {
   // shape of data is statically defined in data.h
-  snax_mac_setup_simple_mult(a, b, d, N);
+  // printf("%x\n", *((uint32_t*)a->shape[0]));
+  snax_mac_setup_simple_mult(a->aligned_data, b->aligned_data, d->aligned_data,
+                             a->shape[0]);
   snax_mac_launch();
   snax_mac_sw_barrier();
 }
@@ -20,14 +35,43 @@ int main() {
   // (snrt_l1_next()) that is the same for all the cores in the cluster, we are
   // essentially providing the same memory regions to all the cores in this
   // cluster.
-  int32_t *local_A = (int32_t *)snrt_l1_next();
-  int32_t *local_B = local_A + N;
-  int32_t *local_D = local_B + N;
+
+  uint32_t constant_zero = 0;
+  uint32_t constant_size = N;
+  // Allocate memory for the fields
+
+  struct OneDMemrefI32 memrefA = {
+      .data = (int32_t *)snrt_l1_next(),
+      .aligned_data = memrefA.data,
+      .offset = &constant_zero,
+      .shape[0] = &constant_size,
+      .stride[0] = &constant_zero,
+  };
+
+  struct OneDMemrefI32 memrefB = {
+      .data = (int32_t *)memrefA.data + N,
+      .aligned_data = memrefB.data,
+      .offset = &constant_zero,
+      .shape[0] = &constant_size,
+      .stride[0] = &constant_zero,
+  };
+
+  struct OneDMemrefI32 memrefD = {
+      .data = (int32_t *)memrefB.data + N,
+      .aligned_data = memrefD.data,
+      .offset = &constant_zero,
+      .shape[0] = &constant_size,
+      .stride[0] = &constant_zero,
+  };
+
+  // int32_t *local_A = (int32_t *)snrt_l1_next();
+  // int32_t *local_B = local_A + N;
+  // int32_t *local_D = local_B + N;
 
   // Copy data in shared local memory
   if (snrt_is_dm_core()) {
-    snrt_dma_start_1d(local_A, A, N * sizeof(float));
-    snrt_dma_start_1d(local_B, B, N * sizeof(float));
+    snrt_dma_start_1d(memrefA.data, A, N * sizeof(int32_t));
+    snrt_dma_start_1d(memrefB.data, B, N * sizeof(int32_t));
   }
 
   snrt_cluster_hw_barrier();
@@ -38,13 +82,13 @@ int main() {
     return 0;
 
   (void)snrt_mcycle();
-  simple_mult(local_A, local_B, local_D);
+  _mlir_ciface_simple_mult(&memrefA, &memrefB, &memrefD);
   (void)snrt_mcycle();
 
   // Correctness check
   int nerr = 0;
   for (int i = 0; i < N; i++) {
-    int32_t error = local_D[i] - G[i];
+    int32_t error = memrefD.data[i] - G[i];
     if (error != 0)
       nerr += 1;
   }
