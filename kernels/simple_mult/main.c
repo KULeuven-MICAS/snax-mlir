01297af8002b0a082c83326a3f061812e57a622a
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2023-11-16 12:15:54 +0100

    revert changes to main

--- kernels/simple_mult/main.c
+++ kernels/simple_mult/main.c
@@ -4,7 +4,6 @@
 #include "stdint.h"
 
 #include <snrt.h>
-#include <stdint.h>
 
 // Kernel provided via external definition
 void _mlir_ciface_simple_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
@@ -18,12 +17,6 @@ void _mlir_ciface_snax_hwpe_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
   snax_mac_sw_barrier();
 }
 
-// Location of matmul operands in L3 TCDM shared across cluster
-int32_t *local_A;
-int32_t *local_B;
-int32_t *local_D;
-int32_t *local_result;
-
 int main() {
   // Allocate shared local memory
   // By avoiding allocators and bumping by a known offset a base pointer
@@ -36,7 +29,7 @@ int main() {
   // Allocate memory for the fields
 
   OneDMemrefI32_t memrefA = {
-      .data = (int32_t *)snrt_l1alloc(N * sizeof(int32_t)),
+      .data = (int32_t *)snrt_l1_next(),
       .aligned_data = memrefA.data,
       .offset = &constant_zero,
       .shape[0] = &constant_size,
@@ -44,7 +37,7 @@ int main() {
   };
 
   OneDMemrefI32_t memrefB = {
-      .data = (int32_t *)snrt_l1alloc(N * sizeof(int32_t)),
+      .data = (int32_t *)memrefA.data + N,
       .aligned_data = memrefB.data,
       .offset = &constant_zero,
       .shape[0] = &constant_size,
@@ -52,7 +45,7 @@ int main() {
   };
 
   OneDMemrefI32_t memrefD = {
-      .data = (int32_t *)snrt_l1alloc(N * sizeof(int32_t)),
+      .data = (int32_t *)memrefB.data + N,
       .aligned_data = memrefD.data,
       .offset = &constant_zero,
       .shape[0] = &constant_size,
@@ -69,48 +62,14 @@ int main() {
 
   snrt_cluster_hw_barrier();
 
-  // section 1 end
-  (void)snrt_mcycle();
-  // section 2 start
-
-  if (!snrt_is_dm_core())
-    _mlir_ciface_simple_mult(&memrefA, &memrefB, &memrefD);
-
-  // section 2 end
-  (void)snrt_mcycle();
-  // section 3 start
-
-  snrt_cluster_hw_barrier();
-
-  // store result back to L1
-
-  if (snrt_is_dm_core()) {
-    snrt_dma_start_1d(D, local_D, N * sizeof(int32_t));
-  }
-
-  snrt_cluster_hw_barrier();
-
-  // section 3 end
-  (void)snrt_mcycle();
-  // section 4 start
-
-  // load result from L1
-  if (snrt_is_dm_core()) {
-    local_result = (int32_t *)snrt_l1alloc(N * sizeof(int32_t));
-    snrt_dma_start_1d(local_result, D, N * sizeof(int32_t));
-  }
-
-  snrt_cluster_hw_barrier();
-
-  // section 4 end
-  (void)snrt_mcycle();
-  // section 5 start
-
-  // do the check with compute core, dm core can return succesfully
-
+  // Launch kernel: from this point on only core 0 is required to be alive.
   int thiscore = snrt_cluster_core_idx();
   if (thiscore != 0)
     return 0;
+
+  (void)snrt_mcycle();
+  _mlir_ciface_simple_mult(&memrefA, &memrefB, &memrefD);
+  (void)snrt_mcycle();
 
   // Correctness check
   int nerr = 0;
@@ -119,9 +78,5 @@ int main() {
     if (error != 0)
       nerr += 1;
   }
-
-  // section 5 end
-  (void)snrt_mcycle();
-
   return nerr;
 }
