d49dbab03e84c7b59a8b1d359c6faf30fa6152d6
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2023-12-04 16:01:17 +0100

    fix alloc kernel

--- kernels/alloc/main.c
+++ kernels/alloc/main.c
@@ -1,25 +1,11 @@
 #include "memref.h"
+#include "snax_rt.h"
 #include "stdint.h"
 #include <snrt.h>
 
 // Kernel provided via external definition
 // The C interface converts the pass by value to a pass by refernece!
 void _mlir_ciface_simple_alloc(OneDMemrefI32_t *returned_alloc);
-
-int8_t *allocated_pointer;
-
-int8_t *_mlir_memref_to_llvm_alloc(uint32_t size) {
-  /* This calls malloc on the DMA core
-   * --> requires mlir opt to compile with:
-   *  --convert-memref-to-llvm="use-generic-functions index-bitwidth=32"
-   * To ensure that all cores in the cluster come up with the correct
-   */
-  if (snrt_is_dm_core()) {
-    allocated_pointer = (int8_t *)snrt_l1alloc(size);
-  }
-  snrt_cluster_hw_barrier();
-  return allocated_pointer;
-};
 
 int main() {
   // Allocate memory for the fields
