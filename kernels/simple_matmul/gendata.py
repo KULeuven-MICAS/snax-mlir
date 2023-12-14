a28c6ddbd25fcb05bca4b082853b3ffeb1ab2bad
Josse Van Delm <josse.vandelm@gmail.com>
2023-12-14 13:17:44 +0100

    Fix data layout transformation in golden model output

--- kernels/simple_matmul/gendata.py
+++ kernels/simple_matmul/gendata.py
@@ -46,23 +46,34 @@ if __name__ == "__main__":
     A_size = [16, 16]
     B_size = [16, 16]
     np.random.seed(0)
-    # G = A*B
+
+    # C = A.B
     A = np.random.randint(low_bound, high_bound, size=A_size, dtype=np.dtype("int8"))
+    # A = np.ones(A_size, dtype=np.dtype("int8"))
+    B = np.random.randint(low_bound, high_bound, size=B_size, dtype=np.dtype("int8"))
+    # B = np.ones(B_size, dtype=np.dtype("int8"))
+    C_golden = np.matmul(A.astype(np.dtype("int32")), B.astype(np.dtype("int32")))
+    C = np.zeros(C_golden.shape, np.dtype("int32"))
+
+    assert A.shape[1] == B.shape[0]
+    sizes = {"N_size": A.shape[0], "K_size": A.shape[1], "M_size": B.shape[1]}
+
+    # Perform layout transformations before writing to memory
     # convert from row-major to block row-major
     A = np.reshape(A, [2, 8, 2, 8])
     # convert to [2,2,8,8]
     A = np.swapaxes(A, 1, 2)
-    B = np.random.randint(low_bound, high_bound, size=B_size, dtype=np.dtype("int8"))
+    B = np.transpose(B)
     # convert from column-major to block column-major
     B = np.reshape(B, [2, 8, 2, 8])
     # convert to [2,2,8,8]
     B = np.swapaxes(B, 1, 2)
-    C_golden = np.matmul(A.astype(np.dtype("int32")), B.astype(np.dtype("int32")))
+    # convert from row-major to block row-major
     C_golden = np.reshape(C_golden, [2, 8, 2, 8])
+    # convert to [2,2,8,8]
     C_golden = np.swapaxes(C_golden, 1, 2)
-    C = np.zeros(C_golden.shape, np.dtype("int32"))
+
     variables = {"A": A, "B": B, "C_golden": C_golden, "C": C}
-    assert A.shape[1] == B.shape[0]
-    sizes = {"N_size": A.shape[0], "K_size": A.shape[1], "M_size": B.shape[1]}
+
     create_header("data.h", sizes, variables)
     create_data("data.c", variables)
