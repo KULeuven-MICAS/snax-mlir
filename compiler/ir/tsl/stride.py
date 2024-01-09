aac5de44990e9568aee5c56e9cbbd3ec4cc32248
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-01-09 09:57:09 +0100

    Add dynamic stride and bound support

--- compiler/ir/tsl/stride.py
+++ compiler/ir/tsl/stride.py
@@ -10,19 +10,29 @@ class Stride:
     used to generate all values within the bound.
 
     Args:
-        stride (int): The stride of the Stride
-        bound (int): The bound of the Stride
+        stride (int | None): The stride of the Stride
+            None represents a dynamic stride
+        bound (int | None): The bound of the Stride
+            None represents a dynamic bound
     """
 
-    stride: int
-    bound: int
+    stride: int | None
+    bound: int | None
+
+    def is_dynamic(self) -> bool:
+        """Check if the Stride is dynamic"""
+        return self.stride is None or self.bound is None
 
     def all_values(self) -> list[int]:
         """Get all values within the bound of the Stride"""
+        if self.is_dynamic():
+            raise ValueError("Cannot get all values of a dynamic stride")
         return list(range(0, self.stride * self.bound, self.stride))
 
     def __str__(self) -> str:
-        return f"{self.stride} x {self.bound}"
+        stride = "?" if self.stride is None else str(self.stride)
+        bound = "?" if self.bound is None else str(self.bound)
+        return f"{stride} x {bound}"
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Stride):
--- tests/ir/tsl/test_tiled_stride.py
+++ tests/ir/tsl/test_tiled_stride.py
@@ -6,20 +6,21 @@ from compiler.ir.tsl.tiled_stride import TiledStride
 
 @pytest.fixture()
 def example_strides():
-    return (Stride(1, 4), Stride(4, 6), Stride(24, 2))
+    return (Stride(1, 4), Stride(4, 6), Stride(24, 2), Stride(None, None))
 
 
 @pytest.fixture()
 def example_tiled_strides(example_strides):
-    stride1, stride2, stride3 = example_strides
+    stride1, stride2, stride3, dynamic_stride = example_strides
     tiledStride1 = TiledStride([stride1, stride2])
     tiledStride2 = TiledStride([stride1, stride2, stride3])
-    return tiledStride1, tiledStride2
+    tiledStride3 = TiledStride([stride1, dynamic_stride])
+    return tiledStride1, tiledStride2, tiledStride3
 
 
 def test_tiled_stride_constructor(example_strides, example_tiled_strides):
-    stride1, stride2, stride3 = example_strides
-    tiledStride1, tiledStride2 = example_tiled_strides
+    stride1, stride2, stride3, _ = example_strides
+    tiledStride1, tiledStride2, _ = example_tiled_strides
     assert tiledStride1.strides[0] == stride1
     assert tiledStride1.strides[1] == stride2
     assert tiledStride2.strides[0] == stride1
@@ -28,20 +29,22 @@ def test_tiled_stride_constructor(example_strides, example_tiled_strides):
 
 
 def test_tiled_stride_depth(example_tiled_strides):
-    tiledStride1, tiledStride2 = example_tiled_strides
+    tiledStride1, tiledStride2, tiledStride3 = example_tiled_strides
     assert tiledStride1.depth() == 2
     assert tiledStride2.depth() == 3
+    assert tiledStride3.depth() == 2
 
 
 def test_tiled_stride_str(example_tiled_strides):
-    tiledStride1, tiledStride2 = example_tiled_strides
+    tiledStride1, tiledStride2, tiledStride3 = example_tiled_strides
     assert str(tiledStride1) == "[1, 4] * [4, 6]"
     assert str(tiledStride2) == "[1, 4, 24] * [4, 6, 2]"
+    assert str(tiledStride3) == "[1, ?] * [4, ?]"
 
 
 def test_tiled_stride_iter(example_strides, example_tiled_strides):
     strides = example_strides
-    _, tiledStride2 = example_tiled_strides
+    _, tiledStride2, _ = example_tiled_strides
 
     for depth, stride in tiledStride2:
         assert isinstance(depth, int)
