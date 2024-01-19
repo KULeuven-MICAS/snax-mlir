b2ba4a816eb6467b7246eca811ea8ec9e5ad92b7
Josse Van Delm <josse.vandelm@gmail.com>
2024-01-19 14:30:03 +0100

    Add support for tflite models from MLPerf Tiny (#59)
    * add models
    
    * add kernels and translated code to linalg
    
    * elementwise kernels for ad
    
    * add elementwise_kernels.mlir
    
    * Add download script that downloads tflite buffer
    
    * Add makefile rules for linalg conversion
    
    * Add bufferization passes
    
    * Remove payload files
    
    * Add makefiles to get models, lock tensorflow version
    
    tf 2.15.0 gives MLIR bytecode compatibility issues with MLIR 17
    
    * Remove more payload files
    
    * Fix formatting
    
    * Add CI flow for MLPerf Tiny benchmarks
    
    ---------
    
    Co-authored-by: Joren Dumoulin <joren.dumoulin@kuleuven.be>

---
+++ runtime/tflite_to_tosa.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+import tensorflow as tf
+
+if __name__ == "__main__":
+    # Check the number of arguments
+    if len(sys.argv) != 5:
+        print("Usage: python tflite_to_tosa.py -c input_source -o output_file")
+        sys.exit(1)
+
+    # Extract the arguments
+    script_name, source_flag, input_source, output_flag, output_file = sys.argv
+
+    # Check if the flags are correct
+    if source_flag != "-c" or output_flag != "-o":
+        print("Invalid flags. Use -c for input source and -o for output file.")
+        sys.exit(1)
+
+    # Your code here to use input_source and output_file
+
+    tf.mlir.experimental.tflite_to_tosa_bytecode(
+        input_source,
+        output_file,
+        use_external_constant=False,
+        ordered_input_arrays=None,
+        ordered_output_arrays=None,
+    )
