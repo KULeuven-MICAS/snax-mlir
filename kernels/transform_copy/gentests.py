c4299765b8831cf485116dbde154ad5105949c0c
Josse Van Delm <josse.vandelm@gmail.com>
2024-02-01 15:13:45 +0100

    mlperf tiny: generalize tensor.pad operations (#69)
    Add --test-linalg-transform-patterns="test-generalize-pad-tensor" 
    to convert tensor.pad to a linalg.generic before bufferization

--- kernels/transform_copy/gentests.py
+++ kernels/transform_copy/gentests.py
@@ -104,5 +104,5 @@ if __name__ == "__main__":
             testcase["tsldst"](size_sqrt),
             testcase["shape"](size_sqrt),
         )
-        with open(testcase["name"] + ".preproc.mlir", "w") as file:
+        with open(testcase["name"] + ".preprocfinal.mlir", "w") as file:
             file.write(mlir)
