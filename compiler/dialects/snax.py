33368b4f5b1dde3865296f18497a906a3ce76ed5
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-02-13 09:51:45 +0100

    dialect: (snax) add reshuffle op (#74)
    * add reshuffle op
    
    * relax layout constraints
    
    * rename reshuffle -> layoutcast

--- compiler/dialects/snax.py
+++ compiler/dialects/snax.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from xdsl.ir import Dialect
-from xdsl.irdl import (
-    IRDLOperation,
-    irdl_op_definition,
-)
+from typing import cast
+
+from xdsl.dialects.memref import MemRefType, UnrankedMemrefType
+from xdsl.ir import Attribute, Dialect, Operation, SSAValue
+from xdsl.irdl import IRDLOperation, irdl_op_definition, operand_def, result_def
+from xdsl.utils.exceptions import VerifyException
 
 
 @irdl_op_definition
@@ -15,4 +16,52 @@ class ClusterSyncOp(IRDLOperation):
     name = "snax.cluster_sync_op"
 
 
-Snax = Dialect("snax", [ClusterSyncOp], [])
+@irdl_op_definition
+class LayoutCast(IRDLOperation):
+    """LayoutCast operation for memrefs in a snax cluster. This
+    operation is used to change the layout of the memref data"""
+
+    name = "snax.layout_cast"
+
+    source = operand_def(MemRefType[Attribute] | UnrankedMemrefType[Attribute])
+    dest = result_def(MemRefType[Attribute] | UnrankedMemrefType[Attribute])
+
+    def __init__(
+        self,
+        source: SSAValue | Operation,
+        dest: MemRefType[Attribute] | UnrankedMemrefType[Attribute],
+    ):
+        super().__init__(operands=[source], result_types=[dest])
+
+    @staticmethod
+    def from_type_and_target_layout(
+        source: SSAValue | Operation,
+        layout: Attribute,
+    ) -> LayoutCast:
+        assert isinstance(source.type, MemRefType)
+        dest = MemRefType(
+            source.type.get_element_type(),
+            shape=source.type.get_shape(),
+            layout=layout,
+            memory_space=source.type.memory_space,
+        )
+        return LayoutCast(source, dest)
+
+    def verify_(self) -> None:
+        source = cast(MemRefType[Attribute], self.source.type)
+        dest = cast(MemRefType[Attribute], self.dest.type)
+        if source.get_shape() != dest.get_shape():
+            raise VerifyException(
+                "Expected source and destination to have the same shape."
+            )
+        if source.get_element_type() != dest.get_element_type():
+            raise VerifyException(
+                "Expected source and destination to have the same element type."
+            )
+        if source.memory_space != dest.memory_space:
+            raise VerifyException(
+                "Expected source and destination to have the same memory space."
+            )
+
+
+Snax = Dialect("snax", [ClusterSyncOp, LayoutCast], [])
---
+++ tests/dialects/test_snax.py
@@ -0,0 +1,64 @@
+import pytest
+from xdsl.dialects import builtin
+from xdsl.dialects.builtin import StridedLayoutAttr, i32, i64
+from xdsl.dialects.memref import MemRefType
+from xdsl.utils.exceptions import VerifyException
+from xdsl.utils.test_value import TestSSAValue
+
+from compiler.dialects.snax import LayoutCast
+
+
+def test_memref_memory_space_cast():
+    layout_1 = StridedLayoutAttr(strides=(2, 4, 6), offset=8)
+    layout_2 = StridedLayoutAttr(strides=(2, 8, 16), offset=4)
+
+    source_type = MemRefType(
+        i32, [10, 2], layout=layout_1, memory_space=builtin.IntegerAttr(1, i32)
+    )
+    source_ssa = TestSSAValue(source_type)
+
+    dest_type = MemRefType(i32, [10, 2], memory_space=builtin.IntegerAttr(1, i32))
+
+    memory_layout_cast = LayoutCast(source_ssa, dest_type)
+
+    assert memory_layout_cast.source is source_ssa
+    assert memory_layout_cast.dest.type is dest_type
+
+    dest_type_other_element = MemRefType(
+        i64, [10, 2], layout=layout_2, memory_space=builtin.IntegerAttr(1, i32)
+    )
+
+    with pytest.raises(
+        VerifyException,
+        match="Expected source and destination to have the same element type.",
+    ):
+        LayoutCast(source_ssa, dest_type_other_element).verify()
+
+    dest_type_other_shape = MemRefType(
+        i32, [10, 4], layout=layout_2, memory_space=builtin.IntegerAttr(1, i32)
+    )
+
+    with pytest.raises(
+        VerifyException, match="Expected source and destination to have the same shape."
+    ):
+        LayoutCast(source_ssa, dest_type_other_shape).verify()
+
+    dest_type_other_space = MemRefType(
+        i32, [10, 2], layout=layout_2, memory_space=builtin.IntegerAttr(2, i32)
+    )
+
+    with pytest.raises(
+        VerifyException,
+        match="Expected source and destination to have the same memory space.",
+    ):
+        LayoutCast(source_ssa, dest_type_other_space).verify()
+
+    type_nolayout = MemRefType(i32, [10, 2], memory_space=builtin.IntegerAttr(1, i32))
+    TestSSAValue(type_nolayout)
+
+    # Test helper function
+    memory_layout_cast = LayoutCast.from_type_and_target_layout(source_ssa, layout_2)
+
+    assert memory_layout_cast.source is source_ssa
+    assert isinstance(memory_layout_cast.dest.type, MemRefType)
+    assert memory_layout_cast.dest.type.layout is layout_2
