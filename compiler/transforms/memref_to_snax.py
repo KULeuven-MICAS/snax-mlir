c9a705b9900a79ca6158c1c9f75359fb607f58a6
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-02-26 10:32:43 +0100

    bugfix: floating point type bitwidth

--- compiler/transforms/memref_to_snax.py
+++ compiler/transforms/memref_to_snax.py
@@ -69,8 +69,12 @@ class AllocOpRewrite(RewritePattern):
 
             # multiply all the dimensions with the element width
             # to get the size we need to allocate
-            assert element_type.width.data % 8 == 0
-            element_size = element_type.width.data // 8
+            if isinstance(element_type, builtin.AnyFloat):
+                element_width = element_type.get_bitwidth()
+            else:
+                element_width = element_type.width
+            assert element_width.data % 8 == 0
+            element_size = element_width.data // 8
             element_size_op = Constant.from_int_and_width(element_size, IndexType())
             total_size_op = element_size_op
             ops_to_add.append(element_size_op)
