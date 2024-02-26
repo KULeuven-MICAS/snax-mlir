65af1c5ebfb76fd777f0eadee453361733832fa5
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-02-26 10:06:00 +0100

    add lower snax alloc to func pass

--- compiler/transforms/snax_to_func.py
+++ compiler/transforms/snax_to_func.py
@@ -1,4 +1,4 @@
-from xdsl.dialects import builtin, func
+from xdsl.dialects import arith, builtin, func, llvm
 from xdsl.ir import MLContext
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
@@ -26,6 +26,82 @@ class InsertFunctionDeclaration(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(self, module_op: builtin.ModuleOp, rewriter: PatternRewriter):
         func_op = func.FuncOp.external("snax_cluster_hw_barrier", [], [])
+        SymbolTable.insert_or_update(module_op, func_op)
+
+
+class AllocToFunc(RewritePattern):
+    """Swap snax.alloc with function call
+
+    Awaiting an llvm.inline_asm snitch runtime, implement the snax.allocs
+    through C interfacing with function calls. The function call returns a
+    pointer to the allocated memory. Aligned allocation is not supported yet,
+    and the argument will be ignored. The pass is only implemented for L1 (TCDM)
+    memory space allocations.
+
+    In this pass we must also initialize the llvm struct with the correct contents
+    for now we only populate pointer, aligned_pointer, offset and shapes.
+    The strides are not pupulated because they are not used, and often not available.
+    """
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, alloc_op: snax.Alloc, rewriter: PatternRewriter):
+        ## only supoorting L1 allocation for now
+        if not (
+            isinstance(alloc_op.memory_space, builtin.IntegerAttr)
+            and alloc_op.memory_space.value.data == 1
+        ):
+            return
+
+        def dense_array(pos):
+            return builtin.DenseArrayBase.create_dense_int_or_index(builtin.i64, pos)
+
+        ops_to_insert = []
+
+        func_call = func.Call(
+            "snax_alloc_l1", [alloc_op.size], [llvm.LLVMPointerType.opaque()]
+        )
+        ops_to_insert.append(func_call)
+
+        llvm_struct = llvm.UndefOp(alloc_op.result.type)
+        ops_to_insert.append(llvm_struct)
+
+        llvm_struct = llvm.InsertValueOp(
+            dense_array([0]), llvm_struct.res, func_call.res[0]
+        )
+        ops_to_insert.append(llvm_struct)
+
+        llvm_struct = llvm.InsertValueOp(
+            dense_array([1]), llvm_struct.res, func_call.res[0]
+        )
+        ops_to_insert.append(llvm_struct)
+
+        cst_zero = arith.Constant.from_int_and_width(0, builtin.i32)
+        llvm_struct = llvm.InsertValueOp(dense_array([2]), llvm_struct.res, cst_zero)
+        ops_to_insert.extend([cst_zero, llvm_struct])
+
+        # use shape operands to populate shape of memref descriptor
+        for i, shape_op in enumerate(alloc_op.shapes):
+            if isinstance(shape_op.type, builtin.IndexType):
+                # we must cast to integer for valid llvm op
+                shape_op = builtin.UnrealizedConversionCastOp.get(
+                    [shape_op], [builtin.i32]
+                )
+                ops_to_insert.append(shape_op)
+            llvm_struct = llvm.InsertValueOp(
+                dense_array([3, i]), llvm_struct.res, shape_op.results[0]
+            )
+            ops_to_insert.append(llvm_struct)
+
+        module_op = alloc_op.get_toplevel_object()
+
+        rewriter.replace_matched_op(ops_to_insert)
+
+        func_op = func.FuncOp.external(
+            "snax_alloc_l1",
+            [builtin.IndexType()],
+            [llvm.LLVMPointerType.opaque()],
+        )
+
         SymbolTable.insert_or_update(module_op, func_op)
 
 
--- tests/filecheck/transforms/snax_to_func.mlir
+++ tests/filecheck/transforms/snax_to_func.mlir
@@ -9,3 +9,26 @@
 //CHECK-NEXT:   "func.func"() <{"sym_name" = "snax_cluster_hw_barrier", "function_type" = () -> (), "sym_visibility" = "private"}> ({
 //CHECK-NEXT:   }) : () -> ()
 //CHECK-NEXT: }) : () -> ()
+
+// -----
+
+"builtin.module"() ({
+  %0 = "test.op"() : () -> (index)
+  %1 = "snax.alloc"(%0, %0, %0) <{"memory_space" = 1 : i32, "alignment" = 64 : i32}> : (index, index, index) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+}) : () -> ()
+
+// CHECK: "builtin.module"() ({
+// CHECK-NEXT:   %0 = "test.op"() : () -> index
+// CHECK-NEXT:   %1 = "func.call"(%0) <{"callee" = @snax_alloc_l1}> : (index) -> !llvm.ptr
+// CHECK-NEXT:   %2 = "llvm.mlir.undef"() : () -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   %3 = "llvm.insertvalue"(%2, %1) <{"position" = array<i64: 0>}> : (!llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>, !llvm.ptr) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   %4 = "llvm.insertvalue"(%3, %1) <{"position" = array<i64: 1>}> : (!llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>, !llvm.ptr) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   %5 = "arith.constant"() <{"value" = 0 : i32}> : () -> i32
+// CHECK-NEXT:   %6 = "llvm.insertvalue"(%4, %5) <{"position" = array<i64: 2>}> : (!llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>, i32) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   %7 = "builtin.unrealized_conversion_cast"(%0) : (index) -> i32
+// CHECK-NEXT:   %8 = "llvm.insertvalue"(%6, %7) <{"position" = array<i64: 3, 0>}> : (!llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>, i32) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   %9 = "builtin.unrealized_conversion_cast"(%0) : (index) -> i32
+// CHECK-NEXT:   %10 = "llvm.insertvalue"(%8, %9) <{"position" = array<i64: 3, 1>}> : (!llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>, i32) -> !llvm.struct<(!llvm.ptr, !llvm.ptr, i32, !llvm.array<2 x i32>, !llvm.array<2 x i32>)>
+// CHECK-NEXT:   "func.func"() <{"sym_name" = "snax_alloc_l1", "function_type" = (index) -> !llvm.ptr, "sym_visibility" = "private"}> ({
+// CHECK-NEXT:   }) : () -> ()
+// CHECK-NEXT: }) : () -> ()
