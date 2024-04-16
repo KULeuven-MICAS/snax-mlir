5bdd695b671cd51a8094999df514780a84f7e037
Josse Van Delm <josse.vandelm@gmail.com>
2024-04-16 18:15:34 +0200

    Add more comments

--- compiler/accelerators/registry.py
+++ compiler/accelerators/registry.py
@@ -3,4 +3,9 @@ from compiler.accelerators.snax_hwpe_mult import SNAXHWPEMultAccelerator
 
 
 def get_registered_accelerators() -> dict[str, type[Accelerator]]:
+    """
+    Get a reference to an Accelerator interface based on a symbol name
+    This registry is used to get specific accelerator conversions from
+    a query based on an acc2.acceleratorop.
+    """
     return {"snax_hwpe_mult": SNAXHWPEMultAccelerator}
