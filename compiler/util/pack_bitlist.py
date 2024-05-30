30db7b191fc42ffac5505093b31d0d6b3f7f929b
Anton Lydike <me@antonlydike.de>
2024-05-30 11:31:25 +0200

    much smart, sush wow

--- compiler/util/pack_bitlist.py
+++ compiler/util/pack_bitlist.py
@@ -1,16 +1,32 @@
 from typing import Sequence, Iterable
-from xdsl.ir import Operation, Attribute
-from xdsl.dialects import arith, builtin
+from xdsl.ir import Operation, SSAValue
+from xdsl.dialects import arith
 
 
-def pack_bitlist(values: Sequence[int], offsets: Sequence[int], dtype : Attribute = builtin.i32) -> Iterable[Operation]:
-    shifted_vals = []
+def pack_bitlist(values: Sequence[int | SSAValue | Operation], offsets: Sequence[int | SSAValue | Operation], dtype : int = 32) -> Iterable[Operation]:
+    """
+    Takes a list of values and offsets, and packs them into a single `dtype` wide value.
+
+    Each value is shifted left by its offset and then all of them are or-ed together.
+    (They are or-ed together in a way that reduces data dependencies and makes it O(log(n)))
+
+    Values and offsets can be passed as either SSA values or integers.
+    """
+    shifted_vals: list[SSAValue] = []
     for int_val, int_off in zip(values, offsets, strict=True):
-        yield (offset := arith.Constant.from_int_and_width(int_off, dtype))
-        yield (value := arith.Constant.from_int_and_width(int_val, dtype))
+        if isinstance(int_off, int):
+            yield (offset := arith.Constant.from_int_and_width(int_off, dtype))
+        else:
+            offset = SSAValue.get(int_off)
+        if isinstance(int_val, int):
+            yield (value := arith.Constant.from_int_and_width(int_val, dtype))
+        else:
+            value = SSAValue.get(int_val)
         yield (shift := arith.ShLI(value, offset))
-        shifted_vals.append(shift)
+        shifted_vals.append(shift.result)
 
-    carry = shifted_vals[0]
-    for val in shifted_vals[1:]:
-        yield (carry := arith.OrI(carry, val))
+    # do a divide-and-conquer algorithm to or the values together in O(log(n)) instead of O(n).
+    while len(shifted_vals) > 1:
+        a, b, *rest = shifted_vals
+        yield (ored_val := arith.OrI(a, b))
+        shifted_vals = [*rest, ored_val.result]
--- tests/util/test_pack_bitlist.py
+++ tests/util/test_pack_bitlist.py
@@ -2,6 +2,8 @@ from io import StringIO
 
 from compiler.util.pack_bitlist import pack_bitlist
 from xdsl.printer import Printer
+from xdsl.dialects import arith, builtin
+
 
 def test_pack_bitlist():
     out = StringIO()
@@ -25,6 +27,37 @@ def test_pack_bitlist():
 %10 = arith.constant 128 : i32
 %11 = arith.shli %10, %9 : i32
 %12 = arith.ori %2, %5 : i32
-%13 = arith.ori %12, %8 : i32
-%14 = arith.ori %13, %11 : i32
+%13 = arith.ori %8, %11 : i32
+%14 = arith.ori %12, %13 : i32
+"""
+
+
+def test_pack_bitlist_mixed_vals():
+    out = StringIO()
+    p = Printer(stream=out)
+    input_vals = [
+        arith.Constant.from_int_and_width(v, builtin.i32) for v in [1, 128, 16]
+    ]
+    for op in input_vals:
+        p.print(op)
+    for op in pack_bitlist(
+            [input_vals[0], input_vals[1], 2, input_vals[1]],
+            [0, 8, input_vals[2], 24],
+    ):
+        p.print(op)
+
+    assert out.getvalue() == """%0 = arith.constant 1 : i32
+%1 = arith.constant 128 : i32
+%2 = arith.constant 16 : i32
+%3 = arith.constant 0 : i32
+%4 = arith.shli %0, %3 : i32
+%5 = arith.constant 8 : i32
+%6 = arith.shli %1, %5 : i32
+%7 = arith.constant 2 : i32
+%8 = arith.shli %7, %2 : i32
+%9 = arith.constant 24 : i32
+%10 = arith.shli %1, %9 : i32
+%11 = arith.ori %4, %6 : i32
+%12 = arith.ori %8, %10 : i32
+%13 = arith.ori %11, %12 : i32
 """
