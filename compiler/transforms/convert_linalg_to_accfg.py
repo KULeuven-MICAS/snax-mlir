badb23c50436bc733e672b32e1a0826c57db1524
Anton Lydike <me@antonlydike.de>
2024-06-03 16:17:10 +0100

    format. black

--- compiler/transforms/convert_linalg_to_accfg.py
+++ compiler/transforms/convert_linalg_to_accfg.py
@@ -268,7 +268,14 @@ def _weave_states_in_region(
                 ):
                     continue
                 # these ops are specifically whitelisted:
-                elif isinstance(op, func.Return | scf.Yield | builtin.UnrealizedConversionCastOp):
+                elif isinstance(
+                    op,
+                    (
+                        func.Return,
+                        scf.Yield,
+                        builtin.UnrealizedConversionCastOp,
+                    ),
+                ):
                     continue
                 # for every other operation, raise a warning and just assume the worst
                 else:
