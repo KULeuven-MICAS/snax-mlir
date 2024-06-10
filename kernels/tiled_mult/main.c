cdc3647f4ca3bd80d453e6f2c065eca0b4f8900f
Josse Van Delm <josse.vandelm@gmail.com>
2024-06-10 17:06:57 +0200

    wip

--- kernels/tiled_mult/main.c
+++ kernels/tiled_mult/main.c
@@ -8,7 +8,7 @@
 #include <stdint.h>
 
 // Kernel provided via external definition
-int _mlir_ciface_simple_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
+void _mlir_ciface_simple_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
                               OneDMemrefI32_t *d);
 
 void _mlir_ciface_snax_hwpe_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
@@ -22,21 +22,21 @@ void _mlir_ciface_snax_hwpe_mult(OneDMemrefI32_t *a, OneDMemrefI32_t *b,
 int main() {
 
   // Create memref objects for data stored in L1
-  OneDMemrefI32_t memrefA;
+  static OneDMemrefI32_t memrefA;
   memrefA.data = &A;
   memrefA.aligned_data = memrefA.data;
   memrefA.offset = 0;
   memrefA.shape[0] = N;
   memrefA.stride[0] = sizeof(int32_t);
 
-  OneDMemrefI32_t memrefB;
+  static OneDMemrefI32_t memrefB;
   memrefB.data = &B;
   memrefB.aligned_data = memrefB.data;
   memrefB.offset = 0;
   memrefB.shape[0] = N;
   memrefB.stride[0] = sizeof(int32_t);
 
-  OneDMemrefI32_t memrefD;
+  static OneDMemrefI32_t memrefD;
   memrefD.data = &D;
   memrefD.aligned_data = memrefD.data;
   memrefD.offset = 0;
@@ -44,8 +44,7 @@ int main() {
   memrefD.stride[0] = sizeof(int32_t);
 
   (void)snrt_mcycle();
-  int32_t status;
-  status = _mlir_ciface_simple_mult(&memrefA, &memrefB, &memrefD);
+  _mlir_ciface_simple_mult(&memrefA, &memrefB, &memrefD);
   (void)snrt_mcycle();
 
   snrt_cluster_hw_barrier();
@@ -54,11 +53,12 @@ int main() {
   // from this point on only core 0 is required to be alive.
   int thiscore = snrt_cluster_core_idx();
   if (thiscore != 0)
-    return status;
+    return 0;
 
-  int nerr = status;
+  int nerr = 0;
   for (int i = 0; i < N; i++) {
     int32_t error = memrefD.aligned_data[i] - G[i];
+    printf("test\n");
     if (error != 0)
       nerr += 1;
   }
