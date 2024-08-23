b1b3f9f7f2fbdd7a08233c8a60ad039a10720d8b
Jonas Crols <jonas.crols@telenet.be>
2024-08-23 14:09:38 +0200

    reset main file

--- kernels/streamer_matmul/main.c
+++ kernels/streamer_matmul/main.c
@@ -33,7 +33,7 @@ uint8_t Batch = 1;
  version. 2 always works. however, it will impact performance significantly as
  computation cost doubles. For benchmarks, set to 1 */
 uint8_t M_param = 2;
-uint8_t K_param = K_size / tileSize;
+uint8_t K_param = 2;
 uint8_t N_param = 2;
 
 // Extracted from datagen.py in snitch_cluster repo
@@ -46,10 +46,6 @@ uint32_t ldC = 512;
 uint32_t strideA = 0;
 uint32_t strideB = 0;
 uint32_t strideC = 0;
-
-void _mlir_ciface_snax_debug_me(int32_t *a, int32_t *b, int32_t *c) {
-  printf("here");
-}
 
 // Kernel provided via external definition
 void _mlir_ciface_streamer_matmul(TwoDMemrefI8_t *a, TwoDMemrefI8_t *b,
@@ -137,8 +133,8 @@ int main() {
     for (int i = 0; i < M_size * N_size; i++) {
       {
         int32_t error = memrefC.aligned_data[i] - C_golden[i];
-        printf("%d) %d -> %d\n", i, (int32_t)memrefC.aligned_data[i],
-               (int32_t)C_golden[i]);
+        // printf("%d) %d -> %d\n", i, (int32_t)memrefC.aligned_data[i],
+        //        (int32_t)C_golden[i]);
         if (error != 0)
           nerr += 1;
       }
