df14324c6a6c44d974003bd310c745db578415be
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-08-30 14:24:06 +0200

    udpate

--- runtime/include/snax_rt.h
+++ runtime/include/snax_rt.h
@@ -52,7 +52,7 @@ void _mlir_ciface_snax_dump_l1() {
   snrt_l1alloc(256);
   // memset all to 0 with DMA
   if (snrt_is_dm_core()) {
-    snrt_dma_start_2d((int32_t*) 0x10000040, (int32_t*) 0x10000000, 64, 64, 0, 16380);
+    snrt_dma_start_2d((int32_t*) 0x10000040, (int32_t*) 0x10000000, 0x40, 0x40, 0, 0x1800);
     snrt_dma_wait_all();
   }
   snrt_cluster_hw_barrier();
