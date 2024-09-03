e1a9e250351e7936b66734556d8c259864bd6d7e
Josse Van Delm <josse.vandelm@gmail.com>
2024-09-03 13:17:10 +0200

    Add remove copy pass and filecheck test
    Also solve weird issue in realize-memref-casts pass

---
+++ compiler/transforms/test_remove_memref_copy.py
@@ -0,0 +1,24 @@
+from xdsl.context import MLContext
+from xdsl.dialects import builtin, memref
+from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
+
+
+class RemoveMemrefCopyPattern(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, memref_copy: memref.CopyOp, rewriter: PatternRewriter):
+        rewriter.erase_matched_op()
+
+
+class RemoveMemrefCopyPass(ModulePass):
+    name = "test-remove-memref-copy"
+
+    def apply(self, ctx: MLContext, module: builtin.ModuleOp) -> None:
+        PatternRewriteWalker(
+            RemoveMemrefCopyPattern(), apply_recursively=False
+        ).rewrite_module(module)
