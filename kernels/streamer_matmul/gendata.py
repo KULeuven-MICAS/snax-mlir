f3b75b2fe7351920a9b1f6c8a70d05e8ff2b5544
Josse Van Delm <josse.vandelm@gmail.com>
2024-09-04 13:58:45 +0200

    Parametrize gendata.py

--- kernels/streamer_matmul/gendata.py
+++ kernels/streamer_matmul/gendata.py
@@ -4,27 +4,40 @@ import numpy as np
 
 from util.gendata import create_data, create_header
 
-if __name__ == "__main__":
+
+def create_test_data(n, m, k, ones=False, random_shape=False):
     # Reset random seed for reproducible behavior
 
     np.random.seed(0)
 
     min_size = 1
     max_size = 4
-    n_random = 16  # 8 * np.random.randint(min_size, max_size)
-    m_random = 16  # 8 * np.random.randint(min_size, max_size)
-    k_random = 16  # 8 * np.random.randint(min_size, max_size)
+    n_random = n
+    m_random = m
+    k_random = k
+
+    if random_shape:
+        n_random *= np.random.randint(min_size, max_size)
+        m_random *= np.random.randint(min_size, max_size)
+        k_random *= np.random.randint(min_size, max_size)
+
     A_size = [n_random, k_random]
     B_size = [k_random, m_random]
 
     # C = A.B
     low_bound = -128
     high_bound = 127
-    A = np.random.randint(low_bound, high_bound, size=A_size, dtype=np.dtype("int8"))
-    B = np.random.randint(low_bound, high_bound, size=B_size, dtype=np.dtype("int8"))
 
-    # A = np.ones(shape=A_size, dtype=np.dtype("int8"))
-    # B = np.ones(shape=B_size, dtype=np.dtype("int8"))
+    if ones:
+        A = np.ones(shape=A_size, dtype=np.dtype("int8"))
+        B = np.ones(shape=B_size, dtype=np.dtype("int8"))
+    else:
+        A = np.random.randint(
+            low_bound, high_bound, size=A_size, dtype=np.dtype("int8")
+        )
+        B = np.random.randint(
+            low_bound, high_bound, size=B_size, dtype=np.dtype("int8")
+        )
 
     # Make sure the product is possible!
     assert A.shape[1] == B.shape[0]
@@ -51,3 +64,7 @@ if __name__ == "__main__":
 
     create_header("data.h", sizes, variables)
     create_data("data.c", variables)
+
+
+if __name__ == "__main__":
+    create_test_data(n=16, m=16, k=16)
