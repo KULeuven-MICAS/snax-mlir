10cf79edc3314549aac8dd2fcb40c1a44b75023f
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-09-19 08:59:37 +0200

    finish pass

--- compiler/transforms/frontend/preprocess_mlperf_tiny.py
+++ compiler/transforms/frontend/preprocess_mlperf_tiny.py
@@ -6,15 +6,19 @@ from xdsl.dialects import arith, builtin, func, linalg, memref, tensor
 from xdsl.ir import BlockArgument, Operation, OpResult
 from xdsl.ir.affine import AffineMap
 from xdsl.passes import ModulePass
-from xdsl.pattern_rewriter import PatternRewriter, PatternRewriteWalker, RewritePattern, op_type_rewrite_pattern
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
 from xdsl.rewriter import InsertPoint
 from xdsl.traits import SymbolTable
 from xdsl.transforms.mlir_opt import MLIROptPass
 
 from compiler.dialects import snax
-from compiler.transforms.convert_tosa_to_kernel import ConvertTosaToKernelPass, RescaleClampPattern
+from compiler.transforms.convert_tosa_to_kernel import RescaleClampPattern
 from compiler.transforms.test.insert_debugs import InsertDebugStatements
-from compiler.util.kernel_type import KernelType
 
 
 class InsertStaticFunctionCall(RewritePattern):
@@ -23,7 +27,7 @@ class InsertStaticFunctionCall(RewritePattern):
         """
         Insert a function with static arguments them to dynamic ones,
         calls the existing main function, and then casts the dyanmic output
-        to a static value. After inlining, everything is static then.
+        to a static value. After inlining and canonicalization, everything is static.
         """
 
         # define types
@@ -41,13 +45,20 @@ class InsertStaticFunctionCall(RewritePattern):
             func.Return(cast2)
 
         main_func = func.FuncOp(
-            "run_network", builtin.FunctionType.from_lists([static_input], [static_output]), region=func_region
+            "run_network",
+            builtin.FunctionType.from_lists([static_input], [static_output]),
+            region=func_region,
         )
 
         rewriter.insert_op(main_func, InsertPoint.at_end(op.body.block))
 
 
 class DropOldFunction(RewritePattern):
+    """
+    After the function `main` has been inlined in the function
+    `run_network`, the main function can be dropped
+    """
+
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: func.FuncOp, rewriter: PatternRewriter):
         ## delete old main function which has been inlined
@@ -56,10 +67,12 @@ class DropOldFunction(RewritePattern):
 
 
 class AllocToGlobal(RewritePattern):
-    index: int = 0
     """
     Convert all static allocs to empty statically scheduled memref globals
     """
+
+    # keep track of index to give memref globals a name
+    index: int = 0
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: memref.Alloc, rewriter: PatternRewriter):
@@ -81,13 +94,17 @@ class AllocToGlobal(RewritePattern):
 
         # create global
         memref_global = memref.Global.get(
-            builtin.StringAttr(global_sym_name), op.results[0].type, initial_value=builtin.UnitAttr()
+            builtin.StringAttr(global_sym_name),
+            op.results[0].type,
+            initial_value=builtin.UnitAttr(),
         )
         SymbolTable.insert_or_update(module_op, memref_global)
 
         # remove all the deallocs
         deallocs = [
-            user_op.operation for user_op in op.results[0].uses if isinstance(user_op.operation, memref.Dealloc)
+            user_op.operation
+            for user_op in op.results[0].uses
+            if isinstance(user_op.operation, memref.Dealloc)
         ]
         for dealloc in deallocs:
             rewriter.erase_op(dealloc)
@@ -99,6 +116,11 @@ class AllocToGlobal(RewritePattern):
 
 
 class RemoveZeroInits(RewritePattern):
+    """
+    These ops initialize memory to zero before doing a matmul.
+    With the gemm accelerator we use, this is not necessary.
+    """
+
     @op_type_rewrite_pattern
     def match_and_rewrite(self, op: linalg.FillOp, rewriter: PatternRewriter):
         # delete fill ops that fill with zero, the accelerator will init from zero by default
@@ -119,9 +141,16 @@ class RemoveZeroInits(RewritePattern):
 
 
 class RemoveTransposeConstants(RewritePattern):
+    """
+    This path finds linalg generic operations that transpose an constant.
+    It then constant folds this operation by transforming the weight directly.
+    """
+
     def transpose_tuple(self, array_tuple: tuple, cols: int, rows: int):
         # Transpose using list comprehension
-        transposed_tuple = tuple(array_tuple[i + j * rows] for i in range(rows) for j in range(cols))
+        transposed_tuple = tuple(
+            array_tuple[i + j * rows] for i in range(rows) for j in range(cols)
+        )
         return transposed_tuple
 
     @op_type_rewrite_pattern
@@ -135,9 +164,13 @@ class RemoveTransposeConstants(RewritePattern):
         # check for transpose:
         if len(op.indexing_maps) != 2:
             return
-        if op.indexing_maps.data[0].data != AffineMap.from_callable(lambda x, y: (y, x)):
+        if op.indexing_maps.data[0].data != AffineMap.from_callable(
+            lambda x, y: (y, x)
+        ):
             return
-        if op.indexing_maps.data[1].data != AffineMap.from_callable(lambda x, y: (x, y)):
+        if op.indexing_maps.data[1].data != AffineMap.from_callable(
+            lambda x, y: (x, y)
+        ):
             return
 
         # is input constant?
@@ -147,12 +180,18 @@ class RemoveTransposeConstants(RewritePattern):
             return
         if not isinstance((const_type := op.inputs[0].type), builtin.TensorType):
             return
-        if not isinstance((dense_attr := const_op.value), builtin.DenseIntOrFPElementsAttr):
+        if not isinstance(
+            (dense_attr := const_op.value), builtin.DenseIntOrFPElementsAttr
+        ):
             return
 
         # transpose const op
-        transposed_data = self.transpose_tuple(dense_attr.data.data, *const_type.get_shape())
-        transposed_dense_attr = builtin.DenseIntOrFPElementsAttr.create_dense_int(op.outputs[0].type, transposed_data)
+        transposed_data = self.transpose_tuple(
+            dense_attr.data.data, *const_type.get_shape()
+        )
+        transposed_dense_attr = builtin.DenseIntOrFPElementsAttr.create_dense_int(
+            op.outputs[0].type, transposed_data
+        )
 
         # create new const_op
         new_const_op = arith.Constant(transposed_dense_attr, op.outputs[0].type)
@@ -170,43 +209,10 @@ class RemoveTransposeConstants(RewritePattern):
         pass
 
 
-class RemoveIdioticClamps(RewritePattern):
-    """
-    Sometimes there is a clamping operation that clamps i8 values between -128 and 127
-    This is completely idiotic. This pass removes that operation
-    """
-
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: linalg.Generic, rewriter: PatternRewriter):
-        kernel_type = KernelType.get_kernel(op)
-
-        # if kernel_type != KernelType.CLAMP:
-        #    return
-
-        assert op.body.block.last_op
-        _, lower, upper = KernelType.parse_clamp(op.body.block.last_op.operands[0].op)  # pyright: ignore
-
-        if upper.result.op.value.value.data < 127:
-            return
-
-        if lower.result.op.value.value.data > -128:
-            return
-
-        if op.inputs[0].type.element_type.bitwidth != 8:
-            return
-
-        # otherwise, it is completely useless
-
-        rewriter._replace_all_uses_with(op.results[0], op.inputs[0])
-        rewriter.erase_matched_op()
-
-        pass
-
-
-class InsertMemoryDumps(RewritePattern):
+class InsertMemoryClears(RewritePattern):
     """
     Until we have an established way of handling memory allocation,
-    just dump all memory before every linalg.generic.
+    just clear all memory before every linalg.generic.
     """
 
     @op_type_rewrite_pattern
@@ -219,14 +225,16 @@ class OrganizeGetGlobals(RewritePattern):
     """
     Put getglobals right before their first use
     Otherwise they get allocated too early, and the stupid
-    memory dumps will fuck up this shit
+    memory clears will fuck up this shit
     """
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, getglobal: memref.GetGlobal, rewriter: PatternRewriter):
         assert getglobal.parent
         for firstuser in getglobal.parent.walk():
-            if firstuser in {x.operation for x in getglobal.memref.uses} and isinstance(firstuser, Operation):
+            if firstuser in {x.operation for x in getglobal.memref.uses} and isinstance(
+                firstuser, Operation
+            ):
                 while firstuser.parent != getglobal.parent:
                     assert firstuser.parent
                     firstuser = firstuser.parent
@@ -255,20 +263,12 @@ class PreprocessMLPerfTiny(ModulePass):
 
     mlir_lowering_pass = MLIROptPass(
         arguments=(
-            "--pass-pipeline=builtin.module(func.func(tosa-to-linalg-named, tosa-to-tensor, tosa-to-scf, tosa-to-linalg))",
+            "--pass-pipeline=builtin.module("
+            + "func.func(tosa-to-linalg-named, tosa-to-tensor,"
+            + "tosa-to-scf, tosa-to-linalg, tosa-to-arith, canonicalize))",
             "--mlir-print-op-generic",
             "--mlir-print-local-scope",
             "--allow-unregistered-dialect",
-        )
-    )
-
-    mlir_canonicalization_pass = MLIROptPass(
-        arguments=(
-            "-cse",
-            "-canonicalize",
-            "-mlir-print-local-scope",
-            "-mlir-print-op-generic",
-            "--allow-unregistered-dialect"
         )
     )
 
@@ -281,24 +281,32 @@ class PreprocessMLPerfTiny(ModulePass):
             + " function-boundary-type-conversion=identity-layout-map",
             "--mlir-print-op-generic",
             "--mlir-print-local-scope",
+            "--allow-unregistered-dialect",
         )
     )
 
     def apply(self, ctx: MLContext, op: builtin.ModuleOp) -> None:
-        PatternRewriteWalker(InsertStaticFunctionCall(), apply_recursively=False).rewrite_module(op)
+        PatternRewriteWalker(
+            InsertStaticFunctionCall(), apply_recursively=False
+        ).rewrite_module(op)
         self.mlir_inliner_pass.apply(ctx, op)
         PatternRewriteWalker(DropOldFunction()).rewrite_module(op)
         PatternRewriteWalker(RescaleClampPattern()).rewrite_module(op)
-        #self.mlir_lowering_pass.apply(ctx, op)
-        #self.mlir_canonicalization_pass.apply(ctx, op)
-        # PatternRewriteWalker(RemoveZeroInits(), apply_recursively=False).rewrite_module(op)
-        # PatternRewriteWalker(RemoveTransposeConstants(), apply_recursively=False).rewrite_module(op)
-        # PatternRewriteWalker(RemoveIdioticClamps(), apply_recursively=False).rewrite_module(op)
-        # PatternRewriteWalker(InsertStaticFunctionCall(), apply_recursively=False).rewrite_module(op)
-        # self.mlir_inliner_pass.apply(ctx, op)
-        # PatternRewriteWalker(DropOldFunction()).rewrite_module(op)
-        # self.mlir_bufferization_pass.apply(ctx, op)
-        # PatternRewriteWalker(AllocToGlobal()).rewrite_module(op)
-        # PatternRewriteWalker(InsertMemoryDumps(), apply_recursively=False).rewrite_module(op)
-        # PatternRewriteWalker(InsertDebugStatements(), apply_recursively=False).rewrite_module(op)
-        # PatternRewriteWalker(OrganizeGetGlobals(), apply_recursively=False).rewrite_module(op)
+        self.mlir_lowering_pass.apply(ctx, op)
+        PatternRewriteWalker(RemoveZeroInits(), apply_recursively=False).rewrite_module(
+            op
+        )
+        PatternRewriteWalker(
+            RemoveTransposeConstants(), apply_recursively=False
+        ).rewrite_module(op)
+        self.mlir_bufferization_pass.apply(ctx, op)
+        PatternRewriteWalker(AllocToGlobal()).rewrite_module(op)
+        PatternRewriteWalker(
+            InsertMemoryClears(), apply_recursively=False
+        ).rewrite_module(op)
+        PatternRewriteWalker(
+            InsertDebugStatements(), apply_recursively=False
+        ).rewrite_module(op)
+        PatternRewriteWalker(
+            OrganizeGetGlobals(), apply_recursively=False
+        ).rewrite_module(op)
