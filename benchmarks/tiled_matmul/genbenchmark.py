b4645b42a4924a7492e8c9f1932762647bd8a120
Josse Van Delm <josse.vandelm@gmail.com>
2024-09-20 14:37:49 +0200

    Remove comments

--- benchmarks/tiled_matmul/genbenchmark.py
+++ benchmarks/tiled_matmul/genbenchmark.py
@@ -129,29 +129,3 @@ if __name__ == "__main__":
         bm.process_traces(folder)
         bm.copy_binary(folder)
         bm.copy_logs(folder)
-
-
-#    def run_all(binary: str, folder: str):
-#        bm = SNAXBenchmark(
-#            kernel="streamer_matmul",
-#            binary=binary,
-#            src_dir=str(pathlib.Path.cwd()),
-#            export_dir=str(pathlib.Path.cwd()),
-#        )
-#        bm.clean()
-#        bm.build(build_opts=[])
-#        bm.run()
-#        bm.trace()
-#        bm.process_traces(folder)
-#        bm.copy_binary(folder)
-#        bm.copy_logs(folder)
-#
-#    binaries = {
-#        "run0": "matmul.x",
-#        "run1": "half_tiled_matmul.x",
-#        "run2": "transform_matmul.x",
-#        "run3": "dynamic_matmul.x",
-#    }
-#
-#    for folder, binary in binaries.items():
-#        run_all(binary, folder)
