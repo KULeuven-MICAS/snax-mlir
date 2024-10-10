fdc0a85d6436fa6a612b5ab57a0262993c636252
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-10-10 12:36:57 +0200

    rebasing

--- compiler/tools/snax_opt_main.py
+++ compiler/tools/snax_opt_main.py
@@ -29,9 +29,6 @@ from compiler.transforms.dispatch_kernels import DispatchKernels
 from compiler.transforms.dispatch_regions import DispatchRegions
 from compiler.transforms.frontend.preprocess_mlperf_tiny import PreprocessMLPerfTiny
 from compiler.transforms.fuse_streaming_regions import FuseStreamingRegions
-from compiler.transforms.guarded_linalg_to_memref_stream import (
-    GuardedLinalgToMemrefStreamPass,
-)
 from compiler.transforms.insert_accfg_op import InsertAccOp
 from compiler.transforms.insert_sync_barrier import InsertSyncBarrier
 from compiler.transforms.linalg_to_library_call import LinalgToLibraryCall
