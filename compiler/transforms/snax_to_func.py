eeb2e8552808560251e3749cf3d60d83454c4286
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-10-25 15:10:38 +0200

    add static allocations

--- compiler/transforms/snax_to_func.py
+++ compiler/transforms/snax_to_func.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
 from xdsl.context import MLContext
 from xdsl.dialects import arith, builtin, func, llvm
+from xdsl.dialects.builtin import i32
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -48,6 +51,7 @@ class ClearL1ToFunc(RewritePattern):
         rewriter.replace_matched_op(func_call)
 
 
+@dataclass
 class AllocToFunc(RewritePattern):
     """Swap snax.alloc with function call
 
@@ -61,6 +65,11 @@ class AllocToFunc(RewritePattern):
     for now we only populate pointer, aligned_pointer, offset and shapes.
     The strides are not populated because they are not used, and often not available.
     """
+
+
+    test_static_alloc: bool | None = False
+
+    base_pointer = 0x1000_0200
 
     @op_type_rewrite_pattern
     def match_and_rewrite(self, alloc_op: snax.Alloc, rewriter: PatternRewriter):
@@ -80,34 +89,53 @@ class AllocToFunc(RewritePattern):
         ops_to_insert.append(alignment_op)
 
         # call allocation function with size and alignment operations
-        func_call = func.Call(
-            "snax_alloc_l1",
-            [alloc_op.size, alignment_op],
-            [llvm.LLVMPointerType.opaque()],
-        )
-        ops_to_insert.append(func_call)
+        if self.test_static_alloc:
+            assert isinstance(cst := alloc_op.size.op, arith.Constant)
+            allocated_pointer = self.base_pointer + cst.value.value.data
+            if allocated_pointer % 256 != 0:
+                allocated_pointer += (256 - allocated_pointer % 256)
+            allocated_cst = arith.Constant.from_int_and_width(allocated_pointer, i32)
+            allocated_ptr = llvm.IntToPtrOp(allocated_cst)
+            self.base_pointer = allocated_pointer
 
-        # the result of the function points to a struct containing 2 pointers:
-        # the allocated pointer and the aligned pointer
-        func_result_type = llvm.LLVMStructType.from_type_list(
-            [
-                llvm.LLVMPointerType.opaque(),  # pointer
-                llvm.LLVMPointerType.opaque(),  # aligned_pointer
-            ]
-        )
+            ops_to_insert.extend([allocated_cst, allocated_ptr])
 
-        # load this struct
-        func_result = llvm.LoadOp(func_call.res[0], func_result_type)
-        ops_to_insert.append(func_result)
+            pointer_result = allocated_ptr.output
+            aligned_result = allocated_ptr.output
 
-        # extract the allocated pointer and aligned pointer from alloc function call
-        pointer_op = llvm.ExtractValueOp(
-            dense_array([0]), func_result.results[0], llvm.LLVMPointerType.opaque()
-        )
-        aligned_pointer_op = llvm.ExtractValueOp(
-            dense_array([1]), func_result.results[0], llvm.LLVMPointerType.opaque()
-        )
-        ops_to_insert.extend([pointer_op, aligned_pointer_op])
+        else:
+
+            func_call = func.Call(
+                "snax_alloc_l1",
+                [alloc_op.size, alignment_op],
+                [llvm.LLVMPointerType.opaque()],
+            )
+            ops_to_insert.append(func_call)
+
+            # the result of the function points to a struct containing 2 pointers:
+            # the allocated pointer and the aligned pointer
+            func_result_type = llvm.LLVMStructType.from_type_list(
+                [
+                    llvm.LLVMPointerType.opaque(),  # pointer
+                    llvm.LLVMPointerType.opaque(),  # aligned_pointer
+                ]
+            )
+
+            # load this struct
+            func_result = llvm.LoadOp(func_call.res[0], func_result_type)
+            ops_to_insert.append(func_result)
+
+            # extract the allocated pointer and aligned pointer from alloc function call
+            pointer_op = llvm.ExtractValueOp(
+                dense_array([0]), func_result.results[0], llvm.LLVMPointerType.opaque()
+            )
+            aligned_pointer_op = llvm.ExtractValueOp(
+                dense_array([1]), func_result.results[0], llvm.LLVMPointerType.opaque()
+            )
+
+            pointer_result = pointer_op.res
+            aligned_result = aligned_pointer_op.res
+            ops_to_insert.extend([pointer_op, aligned_pointer_op])
 
         # create the memref descriptor struct
         llvm_struct = llvm.UndefOp(alloc_op.result.type)
@@ -115,13 +143,13 @@ class AllocToFunc(RewritePattern):
 
         # insert pointer
         llvm_struct = llvm.InsertValueOp(
-            dense_array([0]), llvm_struct.res, pointer_op.res
+            dense_array([0]), llvm_struct.res, pointer_result
         )
         ops_to_insert.append(llvm_struct)
 
         # insert aligned pointer
         llvm_struct = llvm.InsertValueOp(
-            dense_array([1]), llvm_struct.res, aligned_pointer_op.res
+            dense_array([1]), llvm_struct.res, aligned_result
         )
         ops_to_insert.append(llvm_struct)
 
@@ -147,17 +175,22 @@ class AllocToFunc(RewritePattern):
 
         rewriter.replace_matched_op(ops_to_insert)
 
-        func_op = func.FuncOp.external(
-            "snax_alloc_l1",
-            [builtin.IndexType()] * 2,
-            [llvm.LLVMPointerType.opaque()],
-        )
+        if not self.test_static_alloc:
+            func_op = func.FuncOp.external(
+                "snax_alloc_l1",
+                [builtin.IndexType()] * 2,
+                [llvm.LLVMPointerType.opaque()],
+            )
 
-        SymbolTable.insert_or_update(module_op, func_op)
+            SymbolTable.insert_or_update(module_op, func_op)
 
 
+@dataclass(frozen=True)
 class SNAXToFunc(ModulePass):
     name = "snax-to-func"
+
+
+    test_static_alloc: bool | None = False  # amount of cores
 
     def apply(self, ctx: MLContext, op: builtin.ModuleOp) -> None:
         contains_sync = any(
@@ -169,5 +202,5 @@ class SNAXToFunc(ModulePass):
             PatternRewriteWalker(InsertFunctionDeclaration()).rewrite_module(op)
 
         PatternRewriteWalker(
-            GreedyRewritePatternApplier([AllocToFunc(), ClearL1ToFunc()])
+            GreedyRewritePatternApplier([AllocToFunc(self.test_static_alloc), ClearL1ToFunc()])
         ).rewrite_module(op)
