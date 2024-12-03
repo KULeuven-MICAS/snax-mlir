a143a2f47c0f73716ae996046c6eb0b4330d9b93
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2024-12-03 15:49:22 +0100

    another filecheck works

--- compiler/transforms/insert_sync_barrier.py
+++ compiler/transforms/insert_sync_barrier.py
@@ -3,24 +3,24 @@ from xdsl.dialects import builtin
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     PatternRewriter,
-    PatternRewriteWalker,
-    RewritePattern,
-    op_type_rewrite_pattern,
 )
 
 from compiler.dialects import snax
 from compiler.util.dispatching_rules import dispatch_to_compute, dispatch_to_dm
 
 
-class InsertSyncBarrierRewriter(RewritePattern):
-    """The algorithm used for this pass investigates these dependencies across
-    cores to insert synchronization passes at the correct times. This is done by
-    walking through every op in the IR. For every operand used by the operation,
-    we check if it also used by an operation on another core. If this is the case,
-    we must insert a synchronization barrier between the two."""
+class InsertSyncBarrier(ModulePass):
+    """This pass inserts  snax synchronisation barriers in a program.
+    Synchronisation barriers are required when data is shared between
+    multiple cores in a cluster, and the correct data flow of the
+    program must be maintained."""
 
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, module: builtin.ModuleOp, rewriter: PatternRewriter):
+    name = "insert-sync-barrier"
+
+    def apply(self, ctx: MLContext, module: builtin.ModuleOp) -> None:
+
+        rewriter = PatternRewriter(module)
+
         ops_to_sync = []
 
         ## walk the entire module in order
@@ -53,17 +53,3 @@ class InsertSyncBarrierRewriter(RewritePattern):
                         op_use.operation
                     ):
                         ops_to_sync.append(op_use.operation)
-
-
-class InsertSyncBarrier(ModulePass):
-    """This pass inserts  snax synchronisation barriers in a program.
-    Synchronisation barriers are required when data is shared between
-    multiple cores in a cluster, and the correct data flow of the
-    program must be maintained."""
-
-    name = "insert-sync-barrier"
-
-    def apply(self, ctx: MLContext, module: builtin.ModuleOp) -> None:
-        PatternRewriteWalker(
-            InsertSyncBarrierRewriter(), apply_recursively=False
-        ).rewrite_module(module)
