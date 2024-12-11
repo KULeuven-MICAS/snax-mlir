77bfc66d20e5e60207397294f4c264e75db82502
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-11 17:02:31 +0100

    Add support for gemmini kernel

--- kernels/tiled_add/gendata.py
+++ kernels/tiled_add/gendata.py
@@ -1,21 +1,52 @@
 import argparse
+import os
 
 import numpy as np
+import numpy.typing as npt
 
-from util.gendata import create_data, create_header
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Generate data for snax-alu operations."
-    )
-    parser.add_argument(
-        "--array_size", type=int, default=1024, help="Size of the arrays to generate"
-    )
-    args = parser.parse_args()
+def create_header(
+    file_name: str, sizes: dict[str, int], variables: dict[str, npt.NDArray]
+) -> None:
+    if os.path.dirname(file_name):
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, "w") as f:
+        includes = ["#include <stdint.h>", "#pragma once", ""]
+        includes = "\n".join(includes)
+        variables_string = [""]
+        for i, j in sizes.items():
+            variables_string.append(f"#define {i} {j}")
+        variables_string.append("")
+        for i, j in variables.items():
+            variables_string.append(f"extern const {j.dtype}_t {i}[{j.size}];")
+        variables_string = "\n".join(variables_string)
+        f.write(includes)
+        f.write(variables_string)
+        f.write("\n")
 
+
+def create_data(file_name: str, variables: dict[str, npt.NDArray]):
+    includes = [f'#include "{file_name[:-2]}.h"', "", ""]
+    includes = "\n".join(includes)
+    variables = {i: np.reshape(j, j.size) for i, j in variables.items()}
+    if os.path.dirname(file_name):
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, "w") as f:
+        f.write(includes)
+        for variable_name, variable_value in variables.items():
+            f.write(
+                f"const {variable_value.dtype}_t {variable_name}"
+                + f"[{variable_value.size}] = "
+                + "{\n"
+            )
+            variable_str = ["\t" + str(i) for i in variable_value]
+            f.write(",\n".join(variable_str))
+            f.write("\n};\n\n")
+
+
+def generate_data(array_size):
     low_bound = -128
     high_bound = 127
-    array_size = args.array_size
 
     # snax-alu design-time spatial parallelism
     spatial_par = 4
@@ -37,5 +68,16 @@ if __name__ == "__main__":
     sizes = {"MODE": 0, "DATA_LEN": array_size, "LOOP_ITER": loop_iter}
     variables = {"A": A, "B": B, "O": O, "G": G}
 
-    create_header("data.h", sizes, variables)
-    create_data("data.c", variables)
+    create_header(f"data_{array_size}.h", sizes, variables)
+    create_data(f"data_{array_size}.c", variables)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate data for snax-alu operations."
+    )
+    parser.add_argument(
+        "--array_size", type=int, default=1024, help="Size of the arrays to generate"
+    )
+    args = parser.parse_args()
+    generate_data(args.array_size)
