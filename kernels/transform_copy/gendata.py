32403dc8309c364c4974a82156600c7e662bd6f5
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-18 20:20:00 +0100

    Add snakefile for transform-copy

--- kernels/transform_copy/gendata.py
+++ kernels/transform_copy/gendata.py
@@ -1,11 +1,11 @@
-# simple script to generate inputs and expected outputs for simple_mult
 from math import sqrt
 
 import numpy as np
 
 from util.gendata import create_data, create_header
 
-if __name__ == "__main__":
+
+def create_files(filename: str):
     array_size = 64
     A = np.linspace(1, array_size, array_size, dtype=np.int32)
     B = np.reshape(A, [2, 4, 2, 4])
@@ -13,9 +13,5 @@ if __name__ == "__main__":
     B = B.flatten()
     sizes = {"N": array_size, "N_sqrt": sqrt(array_size)}
     variables = {"A": A, "B": B}
-    create_header("transform_copy/data.h", sizes, variables)
-    create_data("transform_copy/data.c", variables)
-    create_header("transform_from_none/data.h", sizes, variables)
-    create_data("transform_from_none/data.c", variables)
-    create_header("transform_from_strided/data.h", sizes, variables)
-    create_data("transform_from_strided/data.c", variables)
+    create_header(f"{filename}.h", sizes, variables)
+    create_data(f"{filename}.c", variables)
