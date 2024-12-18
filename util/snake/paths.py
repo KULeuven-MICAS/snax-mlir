6862cb139f796ecad36d920af9ccebeddc2b9407
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-18 19:19:10 +0100

    Actually add snakemake utils

---
+++ util/snake/paths.py
@@ -0,0 +1,8 @@
+def get_default_paths():
+    return {
+        "cc": "clang",
+        "ld": "clang",
+        "mlir-opt": "mlir-opt",
+        "mlir-translate": "mlir-translate",
+        "snax-opt": "snax-opt",
+    }
