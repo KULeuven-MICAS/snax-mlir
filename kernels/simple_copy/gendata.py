d8c421ed5b168cdc4ecd4a76aa63de821780fa8b
Josse Van Delm <josse.vandelm@gmail.com>
2024-12-20 10:12:03 +0100

    Snakemake: Add simple_copy kernel (#312)
    * Add initial snakefile
    
    * Move simple_copy to snakemake
    
    * Remove makefile and clean up data generation

--- kernels/simple_copy/gendata.py
+++ kernels/simple_copy/gendata.py
@@ -1,10 +1,9 @@
-# simple script to generate inputs and expected outputs for simple_mult
-
 import numpy as np
 
 from util.gendata import create_data, create_header
 
-if __name__ == "__main__":
+
+def create_data_files():
     array_size = 10
     A = np.linspace(1, array_size, array_size, dtype=np.int32)
     sizes = {"N": array_size}
