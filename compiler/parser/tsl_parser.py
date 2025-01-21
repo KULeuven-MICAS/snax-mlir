08d44a2eec7e855a388c6c80aec940c2af5d62cb
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-01-21 08:50:17 +0100

    fix parserstate change

--- compiler/parser/tsl_parser.py
+++ compiler/parser/tsl_parser.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
 
-from xdsl.parser.base_parser import BaseParser, ParserState
+from xdsl.parser.base_parser import BaseParser
+from xdsl.parser.generic_parser import ParserState
 from xdsl.utils.exceptions import ParseError
-from xdsl.utils.lexer import Token
+from xdsl.utils.mlir_lexer import MLIRTokenKind
 
 from compiler.ir.tsl import Stride, TiledStride, TiledStridedLayout
 
 
 class TSLParser(BaseParser):
-    def __init__(self, state: ParserState) -> None:
+    def __init__(self, state: ParserState[MLIRTokenKind]) -> None:
         self._resume_from(state)
 
     def _parse_int_or_question(self, context_msg: str = "") -> int | None:
         """Parse either an integer literal, or a '?'."""
-        if self._parse_optional_token(Token.Kind.QUESTION) is not None:
+        if self._parse_optional_token(MLIRTokenKind.QUESTION) is not None:
             return None
         if (v := self.parse_optional_integer(allow_boolean=False)) is not None:
             return v
@@ -23,22 +24,22 @@ class TSLParser(BaseParser):
         """
         steps ::== `(` steps (`,` steps)* `)`
         """
-        self._parse_token(Token.Kind.L_PAREN, "Expected opening bracket")
+        self._parse_token(MLIRTokenKind.L_PAREN, "Expected opening bracket")
         steps: list[int | None] = []
-        while not self._parse_optional_token(Token.Kind.R_PAREN):
+        while not self._parse_optional_token(MLIRTokenKind.R_PAREN):
             steps.append(self._parse_int_or_question())
-            self._parse_optional_token(Token.Kind.COMMA)
+            self._parse_optional_token(MLIRTokenKind.COMMA)
         return steps
 
     def _parse_bound(self) -> list[int | None]:
         """
         bounds ::== `[` bound (`,` bound)* `]`
         """
-        self._parse_token(Token.Kind.L_SQUARE, "Expected opening bracket")
+        self._parse_token(MLIRTokenKind.L_SQUARE, "Expected opening bracket")
         bounds: list[int | None] = []
-        while not self._parse_optional_token(Token.Kind.R_SQUARE):
+        while not self._parse_optional_token(MLIRTokenKind.R_SQUARE):
             bounds.append(self._parse_int_or_question())
-            self._parse_optional_token(Token.Kind.COMMA)
+            self._parse_optional_token(MLIRTokenKind.COMMA)
         return bounds
 
     def _parse_tiled_stride(self) -> TiledStride:
@@ -46,7 +47,7 @@ class TSLParser(BaseParser):
         tiled-stride ::= bounds `->` strides
         """
         bounds = self._parse_bound()
-        self._parse_token(Token.Kind.ARROW, "Expected arrow")
+        self._parse_token(MLIRTokenKind.ARROW, "Expected arrow")
         steps = self._parse_step()
         if len(steps) != len(bounds):
             raise ParseError(
@@ -62,12 +63,12 @@ class TSLParser(BaseParser):
         tstrides: list[TiledStride] = []
         offset = 0
         while True:
-            if self._current_token.kind == Token.Kind.GREATER:
+            if self._current_token.kind == MLIRTokenKind.GREATER:
                 break
             if self.parse_optional_characters("offset"):
-                self._parse_token(Token.Kind.COLON, "Expected colon")
+                self._parse_token(MLIRTokenKind.COLON, "Expected colon")
                 offset = self.parse_integer()
                 break
             tstrides.append(self._parse_tiled_stride())
-            self._parse_optional_token(Token.Kind.COMMA)
+            self._parse_optional_token(MLIRTokenKind.COMMA)
         return TiledStridedLayout(tstrides, offset=offset)
