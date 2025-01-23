02c0c5e21b85b67bcbb4087e9d6ee0081bd14a78
Josse Van Delm <josse.vandelm@gmail.com>
2025-01-23 18:12:52 +0100

    Stop using get_traces

--- util/snake/paths.py
+++ util/snake/paths.py
@@ -1,6 +1,3 @@
-from collections.abc import Generator, Sequence
-
-
 def get_default_paths() -> dict[str, str]:
     return {
         "cc": "clang",
@@ -9,15 +6,3 @@ def get_default_paths() -> dict[str, str]:
         "mlir-translate": "mlir-translate",
         "snax-opt": "snax-opt",
     }
-
-
-def get_traces(
-    prefixes: Sequence[str],
-    num_chips: int = 1,
-    num_harts: int = 2,
-    extension: str = "dasm",
-) -> Generator[str, None, None]:
-    for prefix in prefixes:
-        for chip_id in range(num_chips):
-            for hart_id in range(num_harts):
-                yield f"{prefix}_trace_chip_{chip_id:02d}_hart_{hart_id:05d}.{extension}"
