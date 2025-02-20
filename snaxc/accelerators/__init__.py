e4542966e22a4286f983e0676693b68d957623f1
Josse Van Delm <josse.vandelm@gmail.com>
2025-02-20 13:16:52 +0100

    WIP

--- snaxc/accelerators/__init__.py
+++ snaxc/accelerators/__init__.py
@@ -1,1 +1,42 @@
+from collections.abc import Callable
+
+from snaxc.accelerators.accelerator import Accelerator
+
 from .util import *
+
+
+def get_all_accelerators() -> dict[str, Callable[[], type[Accelerator]]]:
+    """Return the list of all available passes."""
+
+    def get_snax_alu():
+        from snaxc.accelerators.snax_alu import SNAXAluAccelerator
+
+        return SNAXAluAccelerator
+
+    def get_snax_gemm():
+        from snaxc.accelerators.snax_gemm import SNAXGEMMAccelerator
+
+        return SNAXGEMMAccelerator
+
+    def get_snax_gemmx():
+        from snaxc.accelerators.snax_gemmx import SNAXGEMMXAccelerator
+
+        return SNAXGEMMXAccelerator
+
+    def get_snax_hwpe_mult():
+        from snaxc.accelerators.snax_hwpe_mult import SNAXHWPEMultAccelerator
+
+        return SNAXHWPEMultAccelerator
+
+    def get_gemmini():
+        from snaxc.accelerators.gemmini import GemminiAccelerator
+
+        return GemminiAccelerator
+
+    return {
+        "snax_alu": get_snax_alu,
+        "snax_gemm": get_snax_gemm,
+        "snax_gemmx": get_snax_gemmx,
+        "snax_hwpe_mult": get_snax_hwpe_mult,
+        "gemmini": get_gemmini,
+    }
