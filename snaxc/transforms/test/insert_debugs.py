ae03b727320cec297fab06a7c1d5f3b509631d12
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-05-08 16:24:58 +0200

    transforms: (test-insert-debugs) add support for debugging dart operations (#405)
    * debug statements for dart
    
    * formatting
    
    * add dart debugginf function in streamer alu test

--- snaxc/transforms/test/insert_debugs.py
+++ snaxc/transforms/test/insert_debugs.py
@@ -11,6 +11,7 @@ from xdsl.pattern_rewriter import (
 )
 from xdsl.rewriter import InsertPoint
 
+from snaxc.dialects import dart
 from snaxc.dialects.test import debug
 
 
@@ -53,10 +54,50 @@ class InsertDebugStatements(RewritePattern):
         rewriter.insert_op(debug_after, InsertPoint.after(op))
 
 
+@dataclass(frozen=True)
+class InsertDebugStatementsDart(RewritePattern):
+    """
+    Insert debugs for dart ops
+    """
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self, op: dart.StreamingRegionOpBase, rewriter: PatternRewriter
+    ):
+        kernel_name = "dart"
+
+        memreftype = op.inputs[0].type
+        if not isinstance(memreftype, builtin.MemRefType):
+            return
+
+        if isinstance(memreftype.memory_space, builtin.StringAttr):
+            level = memreftype.memory_space.data
+        else:
+            # defaulting to L3
+            level = "L3"
+
+        # Note, the debug op expects 2 inputs. If only 1 is available,
+        # this just sends the first input twice.
+        input1 = op.inputs[0]
+        input2 = op.inputs[1] if len(op.inputs) >= 2 else op.inputs[0]
+
+        debug_before = debug.DebugLinalgOp(
+            input1, input2, op.outputs[0], kernel_name, "before", level
+        )
+        debug_after = debug.DebugLinalgOp(
+            input1, input2, op.outputs[0], kernel_name, "after", level
+        )
+        rewriter.insert_op(debug_before, InsertPoint.before(op))
+        rewriter.insert_op(debug_after, InsertPoint.after(op))
+
+
 class InsertDebugPass(ModulePass):
     name = "test-insert-debugs"
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         PatternRewriteWalker(
             InsertDebugStatements(), apply_recursively=False
+        ).rewrite_module(op)
+        PatternRewriteWalker(
+            InsertDebugStatementsDart(), apply_recursively=False
         ).rewrite_module(op)
