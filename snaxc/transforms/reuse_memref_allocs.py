6835e4b76a59d07e2fb285329f050233b1a1a996
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-05-12 17:28:34 +0200

    pyright: resolve ruse_memref_allocs

--- snaxc/transforms/reuse_memref_allocs.py
+++ snaxc/transforms/reuse_memref_allocs.py
@@ -1,8 +1,9 @@
 from xdsl.context import Context
 from xdsl.dialects import affine, arith, builtin, memref, scf
-from xdsl.dialects.builtin import IndexType
+from xdsl.dialects.builtin import IndexType, IntegerAttr
 from xdsl.ir import Block, Operation, SSAValue
 from xdsl.ir.affine import AffineConstantExpr
+from xdsl.parser import AffineExpr
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -13,6 +14,7 @@ from xdsl.pattern_rewriter import (
 )
 from xdsl.rewriter import InsertPoint
 from xdsl.traits import Pure
+from xdsl.utils.hints import isa
 
 
 def find_parent_for_loop(op: Operation) -> scf.ForOp | None:
@@ -20,14 +22,14 @@ def find_parent_for_loop(op: Operation) -> scf.ForOp | None:
     Find the parent for-loop of the operation.
     If no for-loop is found, return None.
     """
-    op = op.parent_op()
-    if op is None:
+    if (parent_op := op.parent_op()) is None:
         return None
-    while not isinstance(op, scf.ForOp):
-        if op.parent_op() is None:
+    while not isinstance(parent_op, scf.ForOp):
+        if parent_op.parent_op() is None:
             return None
-        op = op.parent_op()
-    return op
+        parent_op = parent_op.parent_op()
+        assert parent_op is not None
+    return parent_op
 
 
 def is_in_loop(op: Operation) -> bool:
@@ -42,7 +44,9 @@ def defined_outside_loop(op: Operation) -> bool:
     Check if all operands of the operation are defined outside the loop.
     """
     for operand in op.operands:
-        if find_parent_for_loop(operand.owner) is find_parent_for_loop(op):
+        if isinstance(operand.owner, Block):
+            return False
+        elif find_parent_for_loop(operand.owner) is find_parent_for_loop(op):
             return False
     return True
 
@@ -56,9 +60,9 @@ class LoopHoistPureOperations(RewritePattern):
     """
 
     # Add operations that are allowed to be moved, even if they are not pure
-    whitelisted_ops: list[Operation | None]
+    whitelisted_ops: list[type[Operation]]
 
-    def __init__(self, whitelisted_ops: list[Operation | None]) -> None:
+    def __init__(self, whitelisted_ops: list[type[Operation]]) -> None:
         self.whitelisted_ops = whitelisted_ops.copy()
         super().__init__()
 
@@ -97,6 +101,7 @@ class LoopHoistPureOperations(RewritePattern):
         # Only the first for-loop is considered, the algorithm can be repeated to elevate the alloc-op higher.
         if can_move_operation(main_op):
             for_op = find_parent_for_loop(main_op)
+            assert for_op is not None
             main_op.detach()
 
             rewriter.insert_op(main_op, InsertPoint.before(for_op))
@@ -117,7 +122,7 @@ class MoveMemrefDims(RewritePattern):
             """
             return find_parent_for_loop(op) is not find_parent_for_loop(dim_op)
 
-        def can_be_constant(val: SSAValue) -> bool:
+        def can_be_constant(val: int | AffineExpr | SSAValue) -> bool:
             """
             Check if all operands are defined outside for loop or can be derived.
             """
@@ -126,9 +131,12 @@ class MoveMemrefDims(RewritePattern):
                 return True
             if isinstance(val, AffineConstantExpr):
                 return True
+            elif isinstance(val, AffineExpr):
+                return False
 
             # Use the owner of the value to check what kind of operation it is
             expr = val.owner
+            assert isinstance(expr, Operation)
             if before_loop(expr):
                 return True
             return False
@@ -140,6 +148,10 @@ class MoveMemrefDims(RewritePattern):
             if before_loop(dim_op):
                 return True
             memref_op = dim_op.source.owner
+            assert isinstance(memref_op, Operation)
+            index = dim_op.index.owner
+            assert isinstance(dim_op.index.owner, arith.ConstantOp)
+            assert isa(dim_op.index.owner.value, IntegerAttr[IndexType])
             index = int(dim_op.index.owner.value.value.data)
             return memref_op_outside_loop(memref_op, index)
 
@@ -204,7 +216,6 @@ class MoveMemrefDims(RewritePattern):
             """
             if all(
                 [
-                    isinstance(dim_op, memref.DimOp),
                     is_in_loop(dim_op),
                     isinstance(dim_op.index.owner, arith.ConstantOp),
                     not used_by_neither_alloc_nor_subview(dim_op),
@@ -213,13 +224,15 @@ class MoveMemrefDims(RewritePattern):
                 return dimension_outside_loop(dim_op)
             return False
 
-        def get_constant_value_from_other_constant(expr) -> arith.ConstantOp:
+        def get_constant_value_from_other_constant(
+            expr: int | arith.ConstantOp,
+        ) -> arith.ConstantOp:
             """
             Returns the constant value of the expression.
             """
             if isinstance(expr, int):
                 return arith.ConstantOp.from_int_and_width(expr, IndexType())
-            if isinstance(expr, arith.ConstantOp):
+            else:
                 return expr
 
         def get_constant_value_from_affine_min(expr: affine.MinOp) -> arith.ConstantOp:
@@ -230,12 +243,16 @@ class MoveMemrefDims(RewritePattern):
                 expr.map.data.results[0]
             ):
                 return get_constant_value_from_other_constant(
-                    expr.map.data.results[0].value
+                    # TODO: look into this, this seems completely illegal?
+                    expr.map.data.results[0].value  # pyright: ignore
                 )
             if can_be_constant(expr.map.data.results[0]):
+                assert isinstance(expr.map.data.results[0], int | arith.ConstantOp)
                 return get_constant_value_from_other_constant(
-                    expr.map.data.results[0].value
+                    # TODO: look into this, this seems completely illegal?
+                    expr.map.data.results[0].value  # pyright: ignore
                 )
+            raise RuntimeError("no constant value found")
 
         def get_new_dim_op(
             dim_op: memref.DimOp,
@@ -247,12 +264,14 @@ class MoveMemrefDims(RewritePattern):
                 return dim_op
             memref_ssa = dim_op.source
             index_constant = dim_op.index.owner
+            assert isinstance(index_constant, arith.ConstantOp)
             return get_new_memref_op(memref_ssa, index_constant)
 
         def get_new_memref_op(
             memref_ssa: SSAValue, index_constant: arith.ConstantOp
         ) -> memref.DimOp | arith.ConstantOp | affine.MinOp:
             memref_op = memref_ssa.owner
+            assert isa(index_constant.value, IntegerAttr[IndexType])
             index = index_constant.value.value.data
             if isinstance(memref_op, Block):
                 # This happens when the dim is called on an input argument
@@ -269,8 +288,9 @@ class MoveMemrefDims(RewritePattern):
                 ):
                     return new_op
                 else:
+                    assert isinstance(new_op, memref.DimOp)
                     return get_new_dim_op(new_op)
-            AssertionError("This Dim Operation is not replaceable")
+            raise AssertionError("This Dim Operation is not replaceable")
 
         # if the alloc can be moved, detach it from the parent and insert new Alloc Object in front of the for-loop
         # When constants or maximal values where found from inside the loop, we need to insert these before the alloc.
@@ -279,23 +299,16 @@ class MoveMemrefDims(RewritePattern):
             new_dim_op = get_new_dim_op(dim_op)
             if isinstance(new_dim_op, affine.MinOp):
                 temp_dim_op = get_constant_value_from_affine_min(new_dim_op)
-                rewriter._replace_all_uses_with(
-                    new_dim_op.results[0],
-                    temp_dim_op.results[0],
-                )
+                new_dim_op.results[0].replace_by(temp_dim_op.results[0])
                 new_dim_op = temp_dim_op
             if new_dim_op is not dim_op:
-                rewriter._replace_all_uses_with(
-                    # This is a private function of Rewriter Class and should be replaced with a public function,
-                    # but for now it works
-                    dim_op.results[0],
-                    new_dim_op.results[0],
-                )
+                dim_op.results[0].replace_by(new_dim_op.results[0])
             for_op = find_parent_for_loop(dim_op)
 
             if is_in_loop(new_dim_op):
                 new_dim_op.detach()
 
+            assert for_op is not None
             if new_dim_op.parent_op() is None:
                 rewriter.insert_op(new_dim_op, InsertPoint.before(for_op))
             if new_dim_op is not dim_op:
