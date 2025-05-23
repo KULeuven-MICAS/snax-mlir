c3966a4781fa9bdcf6a639c0ccb87963cdc8c13f
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-05-23 14:40:54 +0200

    snake: add trace_to_perfetto rule (#438)

--- util/snake/paths.py
+++ util/snake/paths.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 
 def get_default_paths() -> dict[str, str]:
@@ -14,8 +15,14 @@ def get_default_paths() -> dict[str, str]:
 def get_default_snax_paths() -> dict[str, str]:
     # use CONDA_PREFIX to access pixi env
     gen_trace_path = f"{os.environ['CONDA_PREFIX']}/snax-utils/gen_trace.py"
+    trace_to_perfetto = os.path.join(
+        pathlib.Path(__file__).parent.parent.resolve(),
+        "tracing",
+        "trace_to_perfetto.py",
+    )
     return {
         "python": "python",
         "spike-dasm": "spike-dasm",
         "gen_trace.py": gen_trace_path,
+        "trace_to_perfetto": trace_to_perfetto,
     }
