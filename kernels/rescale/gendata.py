dc4934d33093a6d51a839ef6eddae6bf7f65ad24
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-06-30 09:47:32 +0200

    kernels: (rescale) move out golden model to utils

--- kernels/rescale/gendata.py
+++ kernels/rescale/gendata.py
@@ -1,41 +1,7 @@
 import numpy as np
 
+from util.gemmx.simd_golden_model import postprocessing_simd_golden_model
 from util.gendata import create_data, create_header
-
-
-def postprocessing_simd_golden_model(
-    data_in,
-    input_zp_i,
-    output_zp_i,
-    shift_i,
-    max_int_i,
-    min_int_i,
-    double_round_i,
-    multiplier_i,
-):
-    # Step 1: Subtract input zero point
-    var = data_in - input_zp_i
-
-    # Step 2: Multiply with the multiplier avoiding overflow
-    var = np.int64(var) * np.int64(multiplier_i)
-
-    # Step 3: Right shift
-    var = np.int32(var >> (shift_i - 1))
-
-    # Step 4: Apply double rounding if necessary
-    if double_round_i:
-        var = np.where(var >= 0, var + 1, var - 1)
-
-    # Step 5: Final right shift
-    var = var >> 1
-
-    # Step 6: Add output zero point
-    var = var + output_zp_i
-
-    # Step 7: Clip the values to be within min and max integer range
-    var = np.clip(var, min_int_i, max_int_i)
-
-    return var
 
 
 def create_data_files():
