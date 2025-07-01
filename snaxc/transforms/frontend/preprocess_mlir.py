28cbe9e11d24e3834bb397fbff4f5344d42ffbdd
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-07-01 10:03:41 +0200

    networks: (mlperftiny-ad01) add working network kernel (#512)
    * networks: (mlperftiny-ad01) add working network kernel
    
    * add final target
    
    * add gendata rule
    
    * add main.o rule
    
    * keep compile_c

--- snaxc/transforms/frontend/preprocess_mlir.py
+++ snaxc/transforms/frontend/preprocess_mlir.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass, field
 from xdsl.context import Context
 from xdsl.dialects import builtin
 from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import PatternRewriteWalker
 from xdsl.transforms.mlir_opt import MLIROptPass
+
+from snaxc.transforms.frontend.remove_transpose_constants import RemoveTransposeConstants
 
 MLIR_PREPOC_FLAGS: tuple[tuple[str, ...], ...] = (
     (
@@ -20,18 +23,22 @@ MLIR_PREPOC_FLAGS: tuple[tuple[str, ...], ...] = (
         "--allow-unregistered-dialect",
         "--mlir-print-op-generic",
         "--mlir-print-local-scope",
+        "--allow-unregistered-dialect",
     ),
     (
         "--tosa-to-arith=include-apply-rescale",
         "--allow-unregistered-dialect",
         "--mlir-print-op-generic",
         "--mlir-print-local-scope",
+        "--allow-unregistered-dialect",
     ),
     (
         "--linalg-generalize-named-ops",
         "--allow-unregistered-dialect",
+        "--canonicalize",
         "--mlir-print-op-generic",
         "--mlir-print-local-scope",
+        "--allow-unregistered-dialect",
     ),
 )
 
@@ -45,3 +52,5 @@ class PreprocessPass(ModulePass):
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
         for flags in MLIR_PREPOC_FLAGS:
             MLIROptPass(generic=True, arguments=flags).apply(ctx, op)
+
+        PatternRewriteWalker(RemoveTransposeConstants(), apply_recursively=False).rewrite_module(op)
