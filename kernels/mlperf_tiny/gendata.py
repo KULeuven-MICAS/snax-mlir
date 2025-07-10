88d57bea18bdb06e60400a409654a318a7b1ef81
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-07-10 13:54:21 +0200

    wip

--- kernels/mlperf_tiny/gendata.py
+++ kernels/mlperf_tiny/gendata.py
@@ -8,14 +8,14 @@ from util.gendata import create_header_only
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=str, help="Path to the .json sample data")
-    parser.add_argument("--batch_size", type=int, help="Batch size for the data")
+    parser.add_argument("--batch_size", type=int, default=1, help="Batch size for the data")
     parser.add_argument("--output", type=str, default="data.h", help="Output data file")
     args = parser.parse_args()
     data = json.load(open(args.data))
     create_header_only(
         args.output,
         {
-            "input": np.array(data["input"]["data"] * args.batch_size, dtype=data["input"]["dtype"]),
-            "output": np.array(data["output"]["data"] * args.batch_size, dtype=data["output"]["dtype"]),
+            "input": np.array(data["input"]["data"] * args.batch_size, dtype=data["input"]["dtype"]).flatten(),
+            "output": np.array(data["output"]["data"] * args.batch_size, dtype=data["output"]["dtype"]).flatten(),
         },
     )
