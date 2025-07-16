fb4157c14a378b10ed74165105690f0bc6e7469c
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-07-16 12:21:31 +0200

    transforms: (dispatch-kernels) add secondary dispatch mechanism

--- snaxc/accelerators/dispatching.py
+++ snaxc/accelerators/dispatching.py
@@ -12,6 +12,7 @@ from snaxc.dialects.kernel import KernelOp
 class SupportedKernel:
     kernel_type: type[KernelOp]
     operand_types: Iterable[Attribute]
+    secondary: bool = False
 
 
 @dataclass
