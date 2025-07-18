95af71c4c56ca10e29380b51131d74fbf58e61df
Jonas Crols <jonas.crols@telenet.be>
2025-07-18 11:14:32 +0200

    re-add check for core.accelerators

--- snaxc/tools/config_parser.py
+++ snaxc/tools/config_parser.py
@@ -37,13 +37,14 @@ def parse_config(config: Any) -> AccContext:
             cluster = parse_cluster(value)
             context.register_memory(cluster.memory)
             for core in cluster.cores:
-                for accelerator in core.accelerators:
-                    if accelerator == "snax_gemmx":
-                        context.register_accelerator(SNAXGEMMXAccelerator.name, lambda: SNAXGEMMXAccelerator())
-                    elif accelerator == "snax_alu":
-                        context.register_accelerator(SNAXAluAccelerator.name, lambda: SNAXAluAccelerator())
-                    elif accelerator == "xdma":
-                        context.register_accelerator(SNAXXDMAAccelerator.name, lambda: SNAXXDMAAccelerator())
+                if core.accelerators:
+                    for accelerator in core.accelerators:
+                        if accelerator == "snax_gemmx":
+                            context.register_accelerator(SNAXGEMMXAccelerator.name, lambda: SNAXGEMMXAccelerator())
+                        elif accelerator == "snax_alu":
+                            context.register_accelerator(SNAXAluAccelerator.name, lambda: SNAXAluAccelerator())
+                        elif accelerator == "xdma":
+                            context.register_accelerator(SNAXXDMAAccelerator.name, lambda: SNAXXDMAAccelerator())
         else:
             raise ValueError(f"Unknown config key: {key}")
 
