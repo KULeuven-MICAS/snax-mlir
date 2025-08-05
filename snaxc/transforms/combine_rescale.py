c114e79cc838282507116c88619f78a557661090
Jonas Crols <jonas.crols@telenet.be>
2025-08-05 15:58:02 +0200

    make combine rescale pass

---
+++ snaxc/transforms/combine_rescale.py
@@ -0,0 +1,125 @@
+from dataclasses import dataclass
+
+from xdsl.context import Context
+from xdsl.dialects import builtin, tosa
+from xdsl.dialects.builtin import BoolAttr, DenseArrayBase, IntegerAttr, i1, i8, i32
+from xdsl.ir import Operation
+from xdsl.passes import ModulePass
+from xdsl.pattern_rewriter import (
+    PatternRewriter,
+    PatternRewriteWalker,
+    RewritePattern,
+    op_type_rewrite_pattern,
+)
+
+from snaxc.dialects import kernel
+
+
+@dataclass
+class CombineRescaleRewriter(RewritePattern):
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(
+        self, rescale_op: tosa.RescaleOp | kernel.RescaleOp, rewriter: PatternRewriter
+    ):
+        """Combine consecutive rescale operations into a single rescale operation."""
+
+        rescale_type = type(rescale_op)
+
+        if isinstance(rescale_op.input.owner, Operation):
+            if isinstance(rescale_op.input.owner, rescale_type):
+                # Only combine if the input does not get used anywhere else
+                if len(rescale_op.input.uses) != 1:
+                    return
+                prev_rescale_op = rescale_op.input.owner
+                new_input = prev_rescale_op.input
+                new_output_type = rescale_op.result_types[0]
+
+                prev_multiplier = prev_rescale_op.multiplier.get_values()[0]
+                prev_shift = prev_rescale_op.shift.get_values()[0]
+                current_multiplier = rescale_op.multiplier.get_values()[0]
+                current_shift = rescale_op.shift.get_values()[0]
+                assert isinstance(prev_multiplier, int)
+                assert isinstance(current_multiplier, int)
+                assert isinstance(prev_shift, int)
+                assert isinstance(current_shift, int)
+
+                new_multiplier = prev_multiplier * current_multiplier
+                new_shift = prev_shift + current_shift
+
+                # If the new multiplier is too large, we need to adjust it
+                while new_multiplier >= 2**31:
+                    new_multiplier //= 2
+                    new_shift -= 1
+
+                # Create a new combined rescale operation
+                if isinstance(rescale_op, tosa.RescaleOp):
+                    new_rescale_op = tosa.RescaleOp(
+                        operands=[new_input],
+                        result_types=[new_output_type],
+                        properties={
+                            "input_zp": IntegerAttr(
+                                prev_rescale_op.input_zp.value.data
+                                + rescale_op.input_zp.value.data
+                                * (2**current_shift)
+                                // current_multiplier,
+                                i32,
+                            ),
+                            "output_zp": IntegerAttr(
+                                rescale_op.output_zp.value.data
+                                + prev_rescale_op.output_zp.value.data
+                                * prev_multiplier
+                                // (2**prev_shift),
+                                i32,
+                            ),
+                            "multiplier": DenseArrayBase.from_list(
+                                i32,
+                                [new_multiplier],
+                            ),
+                            "shift": DenseArrayBase.from_list(
+                                i8,
+                                [new_shift],
+                            ),
+                            "scale32": BoolAttr(True, i1),
+                            "double_round": BoolAttr(True, i1),
+                            "per_channel": BoolAttr(False, i1),
+                        },
+                    )
+                else:
+                    new_rescale_op = kernel.RescaleOp(
+                        input=new_input,
+                        result_type=new_output_type,
+                        input_zp=(
+                            prev_rescale_op.input_zp.value.data
+                            + rescale_op.input_zp.value.data
+                            * (2**current_shift)
+                            // current_multiplier
+                        ),
+                        output_zp=(
+                            rescale_op.output_zp.value.data
+                            + prev_rescale_op.output_zp.value.data
+                            * prev_multiplier
+                            // (2**prev_shift)
+                        ),
+                        multiplier=[new_multiplier],
+                        shift=[new_shift],
+                        max_int=rescale_op.max_int.value.data,
+                        min_int=rescale_op.min_int.value.data,
+                        double_round=True,
+                    )
+
+                # Replace the old ops with the new combined op
+                rewriter.replace_op(rescale_op, new_rescale_op)
+                rewriter.erase_op(prev_rescale_op)
+
+
+@dataclass(frozen=True)
+class CombineRescale(ModulePass):
+    name = "combine-rescale"
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        PatternRewriteWalker(
+            CombineRescaleRewriter(),
+            apply_recursively=False,
+            # First elevate outside first for-loop, then move outside the second for-loop (and optionally more)
+        ).rewrite_module(op)
---
+++ tests/filecheck/transforms/combine-rescale.mlir
@@ -0,0 +1,29 @@
+// RUN: snax-opt %s -p combine-rescale --split-input-file | filecheck %s
+
+func.func public @rescale_combined(%arg0: tensor<64xi8>) -> tensor<64xi32> {
+%0 = tosa.rescale %arg0 {double_round = true, input_zp = 38 : i32, multiplier = array<i32: 1073741824>, output_zp = 0 : i32, per_channel = false, scale32 = true, shift = array<i8: 10>} : (tensor<64xi8>) -> tensor<64xi32>
+%1 = tosa.rescale %0 {double_round = true, input_zp = 0 : i32, multiplier = array<i32: 1657902019>, output_zp = 0 : i32, per_channel = false, scale32 = true, shift = array<i8: 33>} : (tensor<64xi32>) -> tensor<64xi32>
+return %1 : tensor<64xi32>
+}
+
+// CHECK:  func.func public @rescale_combined(%arg0 : tensor<64xi8>) -> tensor<64xi32> {
+// CHECK-NEXT:    %0 = tosa.rescale %arg0 {input_zp = 38 : i32, output_zp = 0 : i32, multiplier = array<i32: 1657902019>, shift = array<i8: 13>, scale32 = true, double_round = true, per_channel = false} : (tensor<64xi8>) -> tensor<64xi32>
+// CHECK-NEXT:    func.return %0 : tensor<64xi32>
+// CHECK-NEXT:  }
+
+
+// -----
+func.func public @rescale_combined(%arg0: tensor<64xi8>) -> tensor<64xi32> {
+%0 = tosa.rescale %arg0 {double_round = true, input_zp = 38 : i32, multiplier = array<i32: 1073741824>, output_zp = 0 : i32, per_channel = false, scale32 = true, shift = array<i8: 10>} : (tensor<64xi8>) -> tensor<64xi32>
+%1 = tosa.rescale %0 {double_round = true, input_zp = 0 : i32, multiplier = array<i32: 16>, output_zp = 0 : i32, per_channel = false, scale32 = true, shift = array<i8: 4>} : (tensor<64xi32>) -> tensor<64xi32>
+%2 = tosa.rescale %1 {double_round = true, input_zp = 0 : i32, multiplier = array<i32: 1657902019>, output_zp = 0 : i32, per_channel = false, scale32 = true, shift = array<i8: 33>} : (tensor<64xi32>) -> tensor<64xi32>
+return %2 : tensor<64xi32>
+}
+
+
+// CHECK:  func.func public @rescale_combined(%arg0 : tensor<64xi8>) -> tensor<64xi32> {
+// CHECK-NEXT:    %0 = tosa.rescale %arg0 {input_zp = 38 : i32, output_zp = 0 : i32, multiplier = array<i32: 1657902019>, shift = array<i8: 13>, scale32 = true, double_round = true, per_channel = false} : (tensor<64xi8>) -> tensor<64xi32>
+// CHECK-NEXT:    func.return %0 : tensor<64xi32>
+// CHECK-NEXT:  }
+
+
