8270a40dc05de74432733b09802d50abc6db32fa
Jonas Crols <jonas.crols@telenet.be>
2025-08-05 10:40:20 +0200

    format files

--- snaxc/accelerators/streamers/__init__.py
+++ snaxc/accelerators/streamers/__init__.py
@@ -1,1 +1,6 @@
-from .streamers import HasAddressRemap, HasBroadcast, HasByteMask, HasChannelMask
+from .streamers import (
+    HasAddressRemap,  # pyright: ignore[reportUnusedImport] #noqa: F401
+    HasBroadcast,  # pyright: ignore[reportUnusedImport] #noqa: F401
+    HasByteMask,  # pyright: ignore[reportUnusedImport] #noqa: F401
+    HasChannelMask,  # pyright: ignore[reportUnusedImport] #noqa: F401
+)
