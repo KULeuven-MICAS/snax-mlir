9c9f44cb5181f4efad763dd8ca0b5a73a0fed914
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-08-12 15:31:39 +0200

    add exp_3 plotting and results

--- asplos/exp_3/plot.py
+++ asplos/exp_3/plot.py
@@ -1,7 +1,8 @@
+import re
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
-import re
 
 # Load the data
 with open("summary.txt") as f:
@@ -25,7 +26,7 @@ for line in lines:
         performance = m * n * k / cycles
         records.append(
             {
-                "layout": layout,
+                "Layout": layout,
                 "system": system,
                 "m": m,
                 "n": n,
@@ -39,14 +40,22 @@ for line in lines:
 df = pd.DataFrame(records)
 
 # Average over number of channels (if more than one channel config is present)
-avg_df = df.groupby(["layout", "system"], as_index=False)["performance"].mean()
+avg_df = df.groupby(["Layout", "system"], as_index=False)["performance"].mean()
+
+layout_mapping = {
+    "banked": "Banked",
+    "cyclic": "Tiled",
+    "default": "Row-Major",
+}
+
+avg_df["Layout"] = avg_df["Layout"].map(layout_mapping)
 
 # Plot
 plt.figure(figsize=(5, 3))
-sns.lineplot(data=avg_df, x="system", y="performance", hue="layout", marker="o")
-plt.title("Average Performance by Layout and System")
-plt.ylabel("Performance")
-plt.xlabel("System")
-plt.xticks(rotation=45)
+sns.lineplot(data=avg_df, x="system", y="performance", style="Layout", markers=True, dashes=True, palette=["#0b5394ff"])
+plt.ylabel("Effective OPS/cycle")
+plt.xlabel("FIFO Depth")
+xticks = sorted(avg_df["system"].unique())
+plt.xticks(ticks=xticks, labels=[str(x)[-1] for x in xticks])
 plt.tight_layout()
-plt.savefig("plot.svg")
+plt.savefig("plot.png", dpi=600)
