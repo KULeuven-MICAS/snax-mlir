6b9be246d6c08fff32b342847d3078648b442e7e
Jonas Crols <jonas.crols@telenet.be>
2025-08-20 12:00:22 +0200

    format files

--- snaxc/accelerators/streamers/xdma_kernels/softmax_kernel.py
+++ snaxc/accelerators/streamers/xdma_kernels/softmax_kernel.py
@@ -56,9 +56,7 @@ class SoftMaxXDMA(XDMAKernel):
         template_bounds = (16,)
         return Template(TemplatePattern(template_bounds, tp) for tp in template)
 
-    def get_streamers(
-        self, streamer_config: StreamerConfiguration
-    ) -> Sequence[Streamer]:
+    def get_streamers(self, streamer_config: StreamerConfiguration) -> Sequence[Streamer]:
         """
         Returns the streamers for this DMA extension.
         """
@@ -96,39 +94,19 @@ class SoftMaxXDMA(XDMAKernel):
         assert isinstance(input_snax_stride_pattern, StridePattern)
 
         input_index = None
-        input_bound_list = [
-            bound.data for bound in input_snax_stride_pattern.upper_bounds
-        ]
+        input_bound_list = [bound.data for bound in input_snax_stride_pattern.upper_bounds]
         for i in range(len(input_snax_stride_pattern.upper_bounds)):
             if input_bound_list[i] == vector_length:
                 input_index = i
                 break
-        assert (
-            input_index is not None
-        ), "Input stride pattern must contain vector length"
+        assert input_index is not None, "Input stride pattern must contain vector length"
 
         # Bring the last upper bound and temporal stride to the front
         input_snax_stride_pattern = StridePattern(
-            [
-                bound.data
-                for i, bound in enumerate(input_snax_stride_pattern.upper_bounds)
-                if i == input_index
-            ]
-            + [
-                bound.data
-                for i, bound in enumerate(input_snax_stride_pattern.upper_bounds)
-                if i != input_index
-            ],
-            [
-                stride.data
-                for i, stride in enumerate(input_snax_stride_pattern.temporal_strides)
-                if i == input_index
-            ]
-            + [
-                stride.data
-                for i, stride in enumerate(input_snax_stride_pattern.temporal_strides)
-                if i != input_index
-            ],
+            [bound.data for i, bound in enumerate(input_snax_stride_pattern.upper_bounds) if i == input_index]
+            + [bound.data for i, bound in enumerate(input_snax_stride_pattern.upper_bounds) if i != input_index],
+            [stride.data for i, stride in enumerate(input_snax_stride_pattern.temporal_strides) if i == input_index]
+            + [stride.data for i, stride in enumerate(input_snax_stride_pattern.temporal_strides) if i != input_index],
             input_snax_stride_pattern.spatial_strides,
         )
 
@@ -137,69 +115,37 @@ class SoftMaxXDMA(XDMAKernel):
 
         # Ensure the vector is the inner dimension of the pattern in the output
         output_index = None
-        output_bound_list = [
-            bound.data for bound in output_snax_stride_pattern.upper_bounds
-        ]
+        output_bound_list = [bound.data for bound in output_snax_stride_pattern.upper_bounds]
         for i in range(len(output_snax_stride_pattern.upper_bounds)):
             if output_bound_list[i] == vector_length:
                 output_index = i
                 break
-        assert (
-            output_index is not None
-        ), "Output stride pattern must contain vector length"
+        assert output_index is not None, "Output stride pattern must contain vector length"
         # Bring the last upper bound and temporal stride to the front
         output_snax_stride_pattern = StridePattern(
-            [
-                bound.data
-                for i, bound in enumerate(output_snax_stride_pattern.upper_bounds)
-                if i == output_index
-            ]
+            [bound.data for i, bound in enumerate(output_snax_stride_pattern.upper_bounds) if i == output_index]
+            + [bound.data for i, bound in enumerate(output_snax_stride_pattern.upper_bounds) if i != output_index],
+            [stride.data for i, stride in enumerate(output_snax_stride_pattern.temporal_strides) if i == output_index]
             + [
-                bound.data
-                for i, bound in enumerate(output_snax_stride_pattern.upper_bounds)
-                if i != output_index
-            ],
-            [
-                stride.data
-                for i, stride in enumerate(output_snax_stride_pattern.temporal_strides)
-                if i == output_index
-            ]
-            + [
-                stride.data
-                for i, stride in enumerate(output_snax_stride_pattern.temporal_strides)
-                if i != output_index
+                stride.data for i, stride in enumerate(output_snax_stride_pattern.temporal_strides) if i != output_index
             ],
             output_snax_stride_pattern.spatial_strides,
         )
 
         # Remove the triple repeat stride pattern from output (only needed for input)
         output_index = None
-        output_bound_list = [
-            bound.data for bound in output_snax_stride_pattern.upper_bounds
-        ]
-        output_stride_list = [
-            stride.data for stride in output_snax_stride_pattern.temporal_strides
-        ]
+        output_bound_list = [bound.data for bound in output_snax_stride_pattern.upper_bounds]
+        output_stride_list = [stride.data for stride in output_snax_stride_pattern.temporal_strides]
         # Find the index of the stride pattern that has a value of 3
         for i in range(len(output_bound_list)):
             if output_bound_list[i] == 3 and output_stride_list[i] == 0:
                 output_index = i
                 break
-        assert (
-            output_index is not None
-        ), "Output stride pattern must contain triple repeat stride"
+        assert output_index is not None, "Output stride pattern must contain triple repeat stride"
 
         output_snax_stride_pattern = StridePattern(
-            [
-                bound.data
-                for i, bound in enumerate(output_snax_stride_pattern.upper_bounds)
-                if i != output_index
-            ],
-            [
-                stride.data
-                for i, stride in enumerate(output_snax_stride_pattern.temporal_strides)
-                if i != output_index
-            ],
+            [bound.data for i, bound in enumerate(output_snax_stride_pattern.upper_bounds) if i != output_index],
+            [stride.data for i, stride in enumerate(output_snax_stride_pattern.temporal_strides) if i != output_index],
             output_snax_stride_pattern.spatial_strides,
         )
 
