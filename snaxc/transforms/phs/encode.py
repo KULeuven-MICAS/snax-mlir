b455978781cf4bdb8718208d6b722c61ccd230c0
Josse Van Delm <josse.vandelm@gmail.com>
2025-11-03 15:59:20 +0100

    Add encoding pass

--- snaxc/transforms/phs/encode.py
+++ snaxc/transforms/phs/encode.py
@@ -1,8 +1,10 @@
-from xdsl.dialects import arith, linalg
+from xdsl.context import Context
+from xdsl.dialects import arith, builtin, linalg
 from xdsl.dialects.builtin import AffineMapAttr, FunctionType, ModuleOp, TensorType
 from xdsl.ir import Block, Operation, Region
 from xdsl.ir.affine import AffineExpr, AffineMap
 from xdsl.parser import DenseIntOrFPElementsAttr, Float32Type, SymbolRefAttr
+from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import PatternRewriter, PatternRewriteWalker, RewritePattern, op_type_rewrite_pattern
 from xdsl.rewriter import InsertPoint
 from xdsl.traits import SymbolTable
@@ -10,93 +12,7 @@ from xdsl.traits import SymbolTable
 from snaxc.dialects import phs
 from snaxc.transforms.phs.combine import append_to_abstract_graph
 
-
-def create_generic() -> linalg.GenericOp:
-    # Inputs
-    tensor_type = TensorType(element_type=Float32Type(), shape=[1, 3])
-    tensor_val_type = DenseIntOrFPElementsAttr.from_list(type=tensor_type, data=[1.0, 2.0, 3.1])
-    tensor_cst = arith.ConstantOp(tensor_val_type)
-
-    # Body creation
-    body_block = Block(arg_types=[Float32Type(), Float32Type()])
-    rhs, lhs = body_block.args
-
-    ops = [
-        added := arith.AddfOp(rhs, lhs),
-        final_add := arith.AddfOp(
-            lhs,
-            added,
-        ),
-        linalg.YieldOp(final_add),
-    ]
-    body_block.add_ops(ops)
-    body = Region(body_block)
-
-    # Affine Map Creation
-    d0 = AffineExpr.dimension(0)
-    d1 = AffineExpr.dimension(1)
-
-    indexing_map = AffineMap(num_dims=2, num_symbols=0, results=(d0, d1))
-    im_attr = AffineMapAttr(indexing_map)
-
-    generic = linalg.GenericOp(
-        inputs=[tensor_cst.result, tensor_cst.result],
-        outputs=[],
-        body=body,
-        indexing_maps=[im_attr, im_attr, im_attr],
-        iterator_types=[
-            linalg.IteratorTypeAttr.parallel(),
-            linalg.IteratorTypeAttr.parallel(),
-            linalg.IteratorTypeAttr.parallel(),
-        ],
-        result_types=[tensor_type],
-    )
-    generic.attributes.update({"acc": SymbolRefAttr("accelerator0")})
-    return generic
-
-
-def create_generic2() -> linalg.GenericOp:
-    # Inputs
-    tensor_type = TensorType(element_type=Float32Type(), shape=[1, 3])
-    tensor_val_type = DenseIntOrFPElementsAttr.from_list(type=tensor_type, data=[1.0, 2.0, 3.1])
-    tensor_cst = arith.ConstantOp(tensor_val_type)
-
-    # Body creation
-    body_block = Block(arg_types=[Float32Type(), Float32Type()])
-    rhs, lhs = body_block.args
-
-    ops = [
-        added := arith.MulfOp(rhs, lhs),
-        final_add := arith.AddfOp(
-            lhs,
-            added,
-        ),
-        linalg.YieldOp(final_add),
-    ]
-    body_block.add_ops(ops)
-    body = Region(body_block)
-
-    # Affine Map Creation
-    d0 = AffineExpr.dimension(0)
-    d1 = AffineExpr.dimension(1)
-
-    indexing_map = AffineMap(num_dims=2, num_symbols=0, results=(d0, d1))
-    im_attr = AffineMapAttr(indexing_map)
-
-    generic = linalg.GenericOp(
-        inputs=[tensor_cst.result, tensor_cst.result],
-        outputs=[],
-        body=body,
-        indexing_maps=[im_attr, im_attr, im_attr],
-        iterator_types=[
-            linalg.IteratorTypeAttr.parallel(),
-            linalg.IteratorTypeAttr.parallel(),
-            linalg.IteratorTypeAttr.parallel(),
-        ],
-        result_types=[tensor_type],
-    )
-    generic.attributes.update({"acc": SymbolRefAttr("accelerator0")})
-    return generic
+MAGIC_ATTR_NAME = "phs_acc"
 
 
 class EncodeLinalgGeneric(RewritePattern):
@@ -105,11 +21,11 @@ class EncodeLinalgGeneric(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(self, linalg_op: linalg.GenericOp, rewriter: PatternRewriter):
         # Bail if this accelerator does not have an acc symbol
-        if "acc" not in linalg_op.attributes:
+        if MAGIC_ATTR_NAME not in linalg_op.attributes:
             return
 
         # Bail if the acc symbol is not a SymbolRefAttr
-        acc_symbol_ref = linalg_op.attributes["acc"]
+        acc_symbol_ref = linalg_op.attributes[MAGIC_ATTR_NAME]
         if not isinstance(acc_symbol_ref, SymbolRefAttr):
             return
 
@@ -164,6 +80,7 @@ class EncodeLinalgGeneric(RewritePattern):
         * the first encountered ternary op is assigned to id "_3_0"
         * ...
         """
+        # TODO, add types to this assignment?
         arity = len(op.operands)
         key = f"_{arity}_opnd"
         if key in self._count:
@@ -181,13 +98,107 @@ class EncodeLinalgGeneric(RewritePattern):
         self._count = {}
 
 
-def encode_generic(module_op: ModuleOp) -> ModuleOp:
-    PatternRewriteWalker(EncodeLinalgGeneric(), apply_recursively=False).rewrite_module(module_op)
-    return module_op
+class PhsEncodePass(ModulePass):
+    name = "phs-encode"
+
+    def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        PatternRewriteWalker(EncodeLinalgGeneric(), apply_recursively=False).rewrite_module(op)
 
 
-generic = create_generic()
-module_op = ModuleOp([create_generic(), create_generic2()])
-print(module_op)
-module_op = encode_generic(module_op)
-print(module_op)
+if __name__ == "__main__":
+
+    def create_generic() -> linalg.GenericOp:
+        # Inputs
+        tensor_type = TensorType(element_type=Float32Type(), shape=[1, 3])
+        tensor_val_type = DenseIntOrFPElementsAttr.from_list(type=tensor_type, data=[1.0, 2.0, 3.1])
+        tensor_cst = arith.ConstantOp(tensor_val_type)
+
+        # Body creation
+        body_block = Block(arg_types=[Float32Type(), Float32Type()])
+        rhs, lhs = body_block.args
+
+        ops = [
+            added := arith.AddfOp(rhs, lhs),
+            final_add := arith.AddfOp(
+                lhs,
+                added,
+            ),
+            linalg.YieldOp(final_add),
+        ]
+        body_block.add_ops(ops)
+        body = Region(body_block)
+
+        # Affine Map Creation
+        d0 = AffineExpr.dimension(0)
+        d1 = AffineExpr.dimension(1)
+
+        indexing_map = AffineMap(num_dims=2, num_symbols=0, results=(d0, d1))
+        im_attr = AffineMapAttr(indexing_map)
+
+        generic = linalg.GenericOp(
+            inputs=[tensor_cst.result, tensor_cst.result],
+            outputs=[],
+            body=body,
+            indexing_maps=[im_attr, im_attr, im_attr],
+            iterator_types=[
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.parallel(),
+            ],
+            result_types=[tensor_type],
+        )
+        generic.attributes.update({"acc": SymbolRefAttr("accelerator0")})
+        return generic
+
+    def create_generic2() -> linalg.GenericOp:
+        # Inputs
+        tensor_type = TensorType(element_type=Float32Type(), shape=[1, 3])
+        tensor_val_type = DenseIntOrFPElementsAttr.from_list(type=tensor_type, data=[1.0, 2.0, 3.1])
+        tensor_cst = arith.ConstantOp(tensor_val_type)
+
+        # Body creation
+        body_block = Block(arg_types=[Float32Type(), Float32Type()])
+        rhs, lhs = body_block.args
+
+        ops = [
+            added := arith.MulfOp(rhs, lhs),
+            final_add := arith.AddfOp(
+                lhs,
+                added,
+            ),
+            linalg.YieldOp(final_add),
+        ]
+        body_block.add_ops(ops)
+        body = Region(body_block)
+
+        # Affine Map Creation
+        d0 = AffineExpr.dimension(0)
+        d1 = AffineExpr.dimension(1)
+
+        indexing_map = AffineMap(num_dims=2, num_symbols=0, results=(d0, d1))
+        im_attr = AffineMapAttr(indexing_map)
+
+        generic = linalg.GenericOp(
+            inputs=[tensor_cst.result, tensor_cst.result],
+            outputs=[],
+            body=body,
+            indexing_maps=[im_attr, im_attr, im_attr],
+            iterator_types=[
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.parallel(),
+                linalg.IteratorTypeAttr.parallel(),
+            ],
+            result_types=[tensor_type],
+        )
+        generic.attributes.update({MAGIC_ATTR_NAME: SymbolRefAttr("accelerator0")})
+        return generic
+
+    def encode_generic(module_op: ModuleOp) -> ModuleOp:
+        PatternRewriteWalker(EncodeLinalgGeneric(), apply_recursively=False).rewrite_module(module_op)
+        return module_op
+
+    generic = create_generic()
+    module_op = ModuleOp([create_generic(), create_generic2()])
+    print(module_op)
+    module_op = encode_generic(module_op)
+    print(module_op)
