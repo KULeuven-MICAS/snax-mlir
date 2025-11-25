bf4bf4ed7b2ab4728d321f41565efb499f38bada
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2025-11-25 15:08:15 +0100

    fix second tosat filecheck test

--- snaxc/transforms/combine_rescale.py
+++ snaxc/transforms/combine_rescale.py
@@ -2,8 +2,9 @@ from dataclasses import dataclass
 
 from xdsl.context import Context
 from xdsl.dialects import builtin, tosa
-from xdsl.dialects.builtin import BoolAttr, DenseArrayBase, IntegerAttr, i1, i8, i32
-from xdsl.ir import Operation
+from xdsl.dialects.builtin import DenseIntOrFPElementsAttr, TensorType, i8, i32
+from xdsl.ir import OpResult, Operation
+from xdsl.irdl import Operand
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     PatternRewriter,
@@ -28,15 +29,30 @@ class TosaCombineRescaleRewriter(RewritePattern):
                 new_input = prev_rescale_op.input
                 new_output_type = rescale_op.result_types[0]
 
-                prev_multiplier = prev_rescale_op.multiplier.get_values()[0]
-                prev_shift = prev_rescale_op.shift.get_values()[0]
-                current_multiplier = rescale_op.multiplier.get_values()[0]
-                current_shift = rescale_op.shift.get_values()[0]
+                def extract_const(operand: Operand):
+                    assert isinstance(operand, OpResult)
+                    assert isinstance(const := operand.op, tosa.ConstOp)
+                    return const.values.get_values()
+
+                prev_input_zp = extract_const(prev_rescale_op.input_zp)[0]
+                prev_output_zp = extract_const(prev_rescale_op.output_zp)[0]
+                prev_multiplier = extract_const(prev_rescale_op.multiplier)[0]
+                prev_shift = extract_const(prev_rescale_op.shift)[0]
+                current_input_zp = extract_const(rescale_op.input_zp)[0]
+                current_output_zp = extract_const(rescale_op.output_zp)[0]
+                current_multiplier = extract_const(rescale_op.multiplier)[0]
+                current_shift = extract_const(rescale_op.shift)[0]
+                assert isinstance(prev_input_zp, int)
+                assert isinstance(prev_output_zp, int)
                 assert isinstance(prev_multiplier, int)
-                assert isinstance(current_multiplier, int)
                 assert isinstance(prev_shift, int)
+                assert isinstance(current_input_zp, int)
+                assert isinstance(current_output_zp, int)
+                assert isinstance(current_multiplier, int)
                 assert isinstance(current_shift, int)
 
+                new_input_zp: int = prev_input_zp + current_input_zp * (2**current_shift) // current_multiplier
+                new_output_zp: int = current_output_zp + prev_output_zp * prev_multiplier // (2**prev_shift)
                 new_multiplier = prev_multiplier * current_multiplier
                 new_shift = prev_shift + current_shift
 
@@ -46,36 +62,18 @@ class TosaCombineRescaleRewriter(RewritePattern):
                     new_shift -= 1
 
                 # Create a new combined rescale operation
+                input_zp = tosa.ConstOp(DenseIntOrFPElementsAttr.from_list(TensorType(i32, (1,)), (new_input_zp,)))
+                output_zp = tosa.ConstOp(DenseIntOrFPElementsAttr.from_list(TensorType(i32, (1,)), (new_output_zp,)))
+                multiplier = tosa.ConstOp(DenseIntOrFPElementsAttr.from_list(TensorType(i32, (1,)), (new_multiplier,)))
+                shift = tosa.ConstOp(DenseIntOrFPElementsAttr.from_list(TensorType(i8, (1,)), (new_shift,)))
                 new_rescale_op = tosa.RescaleOp(
-                    operands=[new_input],
+                    operands=[new_input, multiplier, shift, input_zp, output_zp],
                     result_types=[new_output_type],
-                    properties={
-                        "input_zp": IntegerAttr(
-                            prev_rescale_op.input_zp.value.data
-                            + rescale_op.input_zp.value.data * (2**current_shift) // current_multiplier,
-                            i32,
-                        ),
-                        "output_zp": IntegerAttr(
-                            rescale_op.output_zp.value.data
-                            + prev_rescale_op.output_zp.value.data * prev_multiplier // (2**prev_shift),
-                            i32,
-                        ),
-                        "multiplier": DenseArrayBase.from_list(
-                            i32,
-                            [new_multiplier],
-                        ),
-                        "shift": DenseArrayBase.from_list(
-                            i8,
-                            [new_shift],
-                        ),
-                        "scale32": BoolAttr(True, i1),
-                        "double_round": BoolAttr(True, i1),
-                        "per_channel": BoolAttr(False, i1),
-                    },
+                    properties={val: attr for (val, attr) in rescale_op.properties.items()}
                 )
 
                 # Replace the old ops with the new combined op
-                rewriter.replace_op(rescale_op, new_rescale_op)
+                rewriter.replace_op(rescale_op, [input_zp, output_zp, multiplier, shift, new_rescale_op])
                 rewriter.erase_op(prev_rescale_op)
 
 
