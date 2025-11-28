06ac6f25dee3bfd958aebed0fd102bcb58581ac7
Josse Van Delm <josse.vandelm@gmail.com>
2025-11-28 14:04:17 +0100

    Replace arith conversion with circt-opt

--- snaxc/transforms/phs/convert_phs_to_comb.py
+++ snaxc/transforms/phs/convert_phs_to_comb.py
@@ -1,11 +1,12 @@
 from typing import cast
 
 from xdsl.context import Context
-from xdsl.dialects import arith, builtin, comb, hw
+from xdsl.dialects import builtin, comb, hw
 from xdsl.ir import Block, BlockArgument, SSAValue, TypeAttribute
 from xdsl.parser import ArrayAttr
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import PatternRewriter, PatternRewriteWalker, RewritePattern, op_type_rewrite_pattern
+from xdsl.transforms.mlir_opt import MLIROptPass
 
 from snaxc.dialects import phs
 
@@ -18,27 +19,6 @@ class ConvertMuxes(RewritePattern):
         rewriter.replace_value_with_new_type(mux.switch, builtin.IntegerType(1))
         new_mux = comb.MuxOp(mux.switch, mux.rhs, mux.lhs)
         rewriter.replace_op(mux, [new_mux])
-
-
-MaybeCombBinOp = type[comb.BinCombOperation] | type[comb.VariadicCombOperation]
-
-conversion_table: dict[type[arith.SignlessIntegerBinaryOperation], MaybeCombBinOp] = {
-    arith.AddiOp: comb.AddOp,
-    arith.MuliOp: comb.MulOp,
-    arith.DivUIOp: comb.DivUOp,
-    arith.SubiOp: comb.SubOp,
-}
-
-
-class ConvertArithOps(RewritePattern):
-    @op_type_rewrite_pattern
-    def match_and_rewrite(self, bin_op: arith.SignlessIntegerBinaryOperation, rewriter: PatternRewriter):
-        new_op_cls = conversion_table[type(bin_op)]
-        if issubclass(new_op_cls, comb.BinCombOperation):
-            new_op = new_op_cls(operand1=bin_op.lhs, operand2=bin_op.rhs)
-        else:  # issubclass(new_op_cls, comb.VariadicCombOperation)
-            new_op = new_op_cls(input_list=[bin_op.lhs, bin_op.rhs])
-        rewriter.replace_op(bin_op, new_op)
 
 
 class ConvertPeOps(RewritePattern):
@@ -123,8 +103,10 @@ class ConvertPhsToCombPass(ModulePass):
     name = "convert-phs-to-comb"
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        MLIROptPass(
+            executable="circt-opt", generic=True, arguments=("--map-arith-to-comb", "--allow-unregistered-dialect")
+        ).apply(ctx, op)
         PatternRewriteWalker(ConvertPeOps(), apply_recursively=False).rewrite_module(op)
         PatternRewriteWalker(ConvertMuxes(), apply_recursively=False).rewrite_module(op)
-        PatternRewriteWalker(ConvertArithOps(), apply_recursively=False).rewrite_module(op)
         PatternRewriteWalker(ConvertChooseOps(), apply_recursively=False).rewrite_module(op)
         PatternRewriteWalker(ConvertYieldOps(), apply_recursively=False).rewrite_module(op)
