c9c060af58be01e1381dd6a17e7fd639b3aa4f86
Anton Lydike <me@antonlydike.de>
2026-02-06 16:39:25 +0100

    Make work but more so now good

--- snaxc/transforms/phs/convert_float_to_hardfloat.py
+++ snaxc/transforms/phs/convert_float_to_hardfloat.py
@@ -1,5 +1,7 @@
 from collections.abc import Sequence
 from dataclasses import field
+import struct
+from typing import cast
 
 from xdsl.context import Context
 from xdsl.dialects import arith, builtin, hw
@@ -66,6 +68,9 @@ class HWBLockSpec:
 
 i33 = builtin.IntegerType(33)
 i32 = builtin.i32
+i1 = builtin.i1
+i3 = builtin.IntegerType(3)
+i5 = builtin.IntegerType(5)
 
 _HW_BLOCKS = {
     arith.AddfOp: HWBLockSpec("AddRecFN", ("io_a", "io_b"), (i33, i33), ("io_out",), (i33,)),
@@ -74,6 +79,16 @@ _HW_BLOCKS = {
 
 _recode = HWBLockSpec("RecFNFromFN", ("io_in",), (i32,), ("io_out",), (i33,))
 _unrecode = HWBLockSpec("fNFromRecFN", ("io_in",), (i33,), ("io_out",), (i32,))
+_fptosi = HWBLockSpec(
+    "RecFNToIN", ("in", "roundingMode", "signedOut"), (i33, i3, i1), ("out", "intExceptionFlags"), (i32, i3)
+)
+_sitofp = HWBLockSpec(
+    "INToRecFN",
+    ("signedIn", "in", "roundingMode", "detectTininess"),
+    (i1, i32, i3, i1),
+    ("out", "exceptionFlags"),
+    (i33, i5),
+)
 
 
 @dataclass
@@ -87,6 +102,7 @@ class ConvertFloatToHardFloat(RewritePattern):
     """
 
     seen: set[HWBLockSpec] = field(default_factory=set)
+    counter = 0
 
     def match_and_rewrite(self, op: Operation, rewriter: PatternRewriter, /):
         if not isinstance(op, tuple(_HW_BLOCKS)):
@@ -97,24 +113,25 @@ class ConvertFloatToHardFloat(RewritePattern):
         self.seen.add(_recode)
         self.seen.add(_unrecode)
 
-        enc_a, a = ConvertFloatToHardFloat._build_input(op.lhs, "lhs")
+        enc_a, a = ConvertFloatToHardFloat._build_input(op.lhs, f"lhs_{self.counter}")
         if op.lhs is op.rhs:
             enc_b: Sequence[Operation] = []
             b = a
         else:
-            enc_b, b = ConvertFloatToHardFloat._build_input(op.rhs, "rhs")
+            enc_b, b = ConvertFloatToHardFloat._build_input(op.rhs, f"rhs_{self.counter}")
 
         rewriter.replace_op(
             op,
             [
                 *enc_a,
                 *enc_b,
-                actual_op := spec.instance(op.name, [a, b]),
-                res_recoded := _unrecode.instance("unrecoderr", actual_op.results),
+                actual_op := spec.instance(f"{op.name}_{self.counter}", [a, b]),
+                res_recoded := _unrecode.instance(f"unrecoderr_{self.counter}", actual_op.results),
                 ucc := builtin.UnrealizedConversionCastOp.get(res_recoded.results, [op.result.type]),
             ],
             ucc.results,
         )
+        self.counter += 1
 
     @classmethod
     def _build_input(cls, arg: SSAValue, name: str) -> tuple[Sequence[Operation], SSAValue]:
@@ -160,6 +177,70 @@ class CancelUnrecodeRecode(RewritePattern):
             rewriter.erase_op(op_owner)
 
 
+class LowerArithBitcastOp(RewritePattern):
+    """
+    Convert an arith.bitcast op to precisely nothing
+    """
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: arith.BitcastOp, rewriter: PatternRewriter, /):
+        rewriter.replace_op(
+            op, ucc := builtin.UnrealizedConversionCastOp.get(op.operands, [op.result.type]), ucc.results
+        )
+
+
+@dataclass
+class ConvertSiToFPOp(RewritePattern):
+    """
+    Convert an arith.sitofp op with a call to RecFNtoIN
+    """
+
+    seen: set[HWBLockSpec] = field(default_factory=set)
+    counter: int = 0
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: arith.SIToFPOp, rewriter: PatternRewriter, /):
+        self.seen.add(_sitofp)
+        self.seen.add(_unrecode)
+
+        rewriter.replace_op(
+            op,
+            [
+                rnd := arith.ConstantOp(builtin.IntegerAttr(0, i3)),
+                tru := arith.ConstantOp(builtin.IntegerAttr(1, i1)),
+                fals := arith.ConstantOp(builtin.IntegerAttr(0, i1)),
+                cast := _sitofp.instance(f"brrrr_{self.counter}", [tru.result, op.input, rnd.result, fals.result]),
+                i32out := _unrecode.instance(f"unrecode_brrrr_{self.counter}", [cast.results[0]]),
+                ucc := builtin.UnrealizedConversionCastOp.get(i32out.results, [op.result.type]),
+            ],
+            ucc.results,
+        )
+        self.counter += 1
+
+
+class EncodeFloatConstantsPattern(RewritePattern):
+    """
+    Encode floating-point constants as i32 patterns
+    """
+
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: arith.ConstantOp, rewriter: PatternRewriter, /):
+        # this only works on 32-bit floats right now, can't be arsed
+        if op.value.type != builtin.f32:
+            return
+        float_val = float(cast(int | float, op.value.value.data))
+        int_val = cast(int, struct.unpack("i", struct.pack("f", float_val))[0])
+
+        rewriter.replace_op(
+            op,
+            [
+                i32val := arith.ConstantOp(builtin.IntegerAttr(int_val, i32)),
+                ucc := builtin.UnrealizedConversionCastOp.get(i32val.results, [builtin.f32]),
+            ],
+            ucc.results,
+        )
+
+
 class PhsConvertFloatToHardfloatPass(ModulePass):
     """
     Convert arith ops to barkeley hardfloat modules.
@@ -168,18 +249,23 @@ class PhsConvertFloatToHardfloatPass(ModulePass):
     name = "phs-convert-float-to-hardfloat"
 
     def apply(self, ctx: Context, op: builtin.ModuleOp) -> None:
+        seen: set[HWBLockSpec] = set()
+
         PatternRewriteWalker(
             GreedyRewritePatternApplier(
                 [
-                    pattern := ConvertFloatToHardFloat(),
+                    ConvertFloatToHardFloat(seen),
+                    ConvertSiToFPOp(seen),
+                    EncodeFloatConstantsPattern(),
                     # we need to remove ucc as they interfer with the cancellation pass
                     ReconcileUnrealizedCastsPattern(),
                     CancelUnrecodeRecode(),
+                    LowerArithBitcastOp(),
                 ]
             )
         ).rewrite_module(op)
 
         body = op.body.block
         assert body is not None
-        for spec in sorted(pattern.seen, key=lambda spec: spec.symbol_name):
+        for spec in sorted(seen, key=lambda spec: spec.symbol_name):
             body.add_op(spec.module())
