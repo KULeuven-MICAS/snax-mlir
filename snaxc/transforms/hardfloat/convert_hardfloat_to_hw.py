1d2b7ffc6d7a61a7b9dc480f48ead614f82cd4bc
Josse Van Delm <josse.vandelm@gmail.com>
2026-02-20 11:40:56 +0100

    WIP convert-hardfloat-to-hw

--- snaxc/transforms/hardfloat/convert_hardfloat_to_hw.py
+++ snaxc/transforms/hardfloat/convert_hardfloat_to_hw.py
@@ -6,8 +6,8 @@ from typing import cast
 
 from xdsl.context import Context
 from xdsl.dialects import builtin, hw
-from xdsl.dialects.builtin import ModuleOp, SymbolRefAttr
-from xdsl.ir import Attribute, SSAValue, TypeAttribute
+from xdsl.dialects.builtin import IntegerType, ModuleOp, SymbolRefAttr
+from xdsl.ir import Attribute, Operation, SSAValue, TypeAttribute
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -17,10 +17,18 @@ from xdsl.pattern_rewriter import (
     op_type_rewrite_pattern,
 )
 
-from snaxc.dialects import hardfloat
+from snaxc.dialects.hardfloat import (
+    AddRecFnOp,
+    Exception5,
+    FnToRecFnOp,
+    HardfloatOperation,
+    MulRecFnOp,
+    RecFnToFnOp,
+    Rounding,
+)
 
 
-def get_suffix(op: hardfloat.HardfloatOperation) -> str:
+def get_suffix(op: HardfloatOperation) -> str:
     if op.int_width is None:
         return f"_s{op.sig_width.data}_e{op.exp_width.data}"
     else:
@@ -63,30 +71,50 @@ class HWBlockSpec:
         return hw.InstanceOp(
             name,
             self.symbol_attr,
-            tuple(zip(self.in_ports, inputs)),
-            tuple(zip(self.out_ports, [cast(TypeAttribute, out_type) for out_type in self.out_types])),
+            tuple(
+                zip(self.in_ports, inputs, strict=True),
+            ),
+            tuple(
+                zip(
+                    self.out_ports,
+                    [cast(TypeAttribute, out_type) for out_type in self.out_types],
+                )
+            ),
         )
 
     def module(self) -> hw.HWModuleExternOp:
-        mod_type = hw.ModuleType(
-            builtin.ArrayAttr(
-                [
-                    *(
-                        hw.ModulePort(
-                            builtin.StringAttr(p), cast(TypeAttribute, t), hw.DirectionAttr(hw.Direction.INPUT)
-                        )
-                        for p, t in zip(self.in_ports, self.in_types)
-                    ),
-                    *(
-                        hw.ModulePort(
-                            builtin.StringAttr(p), cast(TypeAttribute, t), hw.DirectionAttr(hw.Direction.OUTPUT)
-                        )
-                        for p, t in zip(self.out_ports, self.out_types)
-                    ),
-                ]
+        in_ports: list[hw.ModulePort] = []
+        for p, t in zip(self.in_ports, self.in_types):
+            in_ports.append(
+                hw.ModulePort(builtin.StringAttr(p), cast(TypeAttribute, t), hw.DirectionAttr(hw.Direction.INPUT))
             )
-        )
+        out_ports: list[hw.ModulePort] = []
+        for p, t in zip(self.out_ports, self.out_types):
+            out_ports.append(
+                hw.ModulePort(builtin.StringAttr(p), cast(TypeAttribute, t), hw.DirectionAttr(hw.Direction.OUTPUT))
+            )
+        mod_type = hw.ModuleType(builtin.ArrayAttr([*in_ports, *out_ports]))
         return hw.HWModuleExternOp(builtin.StringAttr(self.symbol_name), mod_type)
+
+    @staticmethod
+    def from_op(op: HardfloatOperation) -> HWBlockSpec:
+        suffix = get_suffix(op)
+        symbol_name = op.get_chisel_name() + suffix
+        # Input names
+        input_names = ["a", "b", "c"][: len(op.operands)]
+        input_types = [*op.operand_types]
+        if Rounding() in op.traits:
+            input_names.extend(["roundingMode", "detectTininess"])
+            input_types.extend([IntegerType(3), IntegerType(1)])
+        io_input_names = (*(f"io_{name}" for name in input_names),)
+        # Output names
+        output_names = ["out"]
+        output_types = [*op.result_types]
+        if Exception5() in op.traits:
+            output_names.extend(["exceptionFlags"])
+            output_types.extend([IntegerType(5)])
+        io_output_names = (*(f"io_{name}" for name in output_names),)
+        return HWBlockSpec(symbol_name, io_input_names, tuple(input_types), io_output_names, tuple(output_types))
 
 
 @dataclass
@@ -96,32 +124,37 @@ class ConvertSimpleOps(RewritePattern):
     @op_type_rewrite_pattern
     def match_and_rewrite(
         self,
-        op: hardfloat.AddRecFnOp
-        | hardfloat.MulRecFnOp
-        | hardfloat.RecFnToFnOp
-        | hardfloat.FnToRecFnOp
-        | hardfloat.RecFnToInOp
-        | hardfloat.InToRecFnOp,
+        op: AddRecFnOp | MulRecFnOp | RecFnToFnOp | FnToRecFnOp,
         rewriter: PatternRewriter,
     ):
-        suffix = get_suffix(op)
         match op:
-            case hardfloat.AddRecFnOp():
-                symbol_name = f"AddRecFN{suffix}"
-            case hardfloat.MulRecFnOp():
-                symbol_name = f"MulRecFN{suffix}"
-            case hardfloat.RecFnToFnOp():
-                symbol_name = f"RecFnToFnOp{suffix}"
-            case hardfloat.FnToRecFnOp():
-                symbol_name = f"FnToRecFnOp{suffix}"
-            case hardfloat.RecFnToInOp():
-                symbol_name = f"RecFnToInOp{suffix}"
-            case hardfloat.InToRecFnOp():
-                symbol_name = f"InToRecFnOp{suffix}"
-        hw_block = HWBlockSpec(symbol_name, ("io_a",), op.operand_types, ("io_out",), op.result_types)
+            case AddRecFnOp():
+                new_ops: list[Operation] = [
+                    c0_i3 := hw.ConstantOp(0, 3),
+                    c0_i1 := hw.ConstantOp(0, 1),
+                ]
+                input_vals = [*op.operands, c0_i3.result, c0_i1.result]
+                hw_block = HWBlockSpec.from_op(op)
+            case MulRecFnOp():
+                new_ops: list[Operation] = [
+                    c0_i3 := hw.ConstantOp(0, 3),
+                    c0_i1 := hw.ConstantOp(0, 1),
+                ]
+                input_vals = [*op.operands, c0_i3.result, c0_i1.result]
+                hw_block = HWBlockSpec.from_op(op)
+            case RecFnToFnOp():
+                new_ops: list[Operation] = []
+                input_vals = op.operands
+                hw_block = HWBlockSpec.from_op(op)
+            case FnToRecFnOp():
+                new_ops: list[Operation] = []
+                input_vals = [*op.operands]
+                hw_block = HWBlockSpec.from_op(op)
+        symbol_name = hw_block.symbol_name
         count = get_count(self.counts, hw_block)
-        instance_op = hw_block.instance(f"{symbol_name}_{count}", [*op.operands])
-        rewriter.replace_op(op, instance_op, new_results=instance_op.results)
+        instance_op = hw_block.instance(f"{symbol_name}_{count}", input_vals)
+        new_ops.append(instance_op)
+        rewriter.replace_op(op, new_ops, new_results=[instance_op.results[0]])
 
 
 class ConvertHardfloatToHw(ModulePass):
