e15850378851a75a88e6897de058142f36ef1934
Josse Van Delm <josse.vandelm@gmail.com>
2026-02-20 11:40:15 +0100

    Completely rework hardfloat dialect for better conversion and rewriting

--- snaxc/transforms/hardfloat/convert_float_to_hardfloat.py
+++ snaxc/transforms/hardfloat/convert_float_to_hardfloat.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import cast
 
 from xdsl.context import Context
-from xdsl.dialects import arith
+from xdsl.dialects import arith, hw
 from xdsl.dialects.builtin import (
     AnyFloat,
     BFloat16Type,
@@ -12,12 +12,9 @@ from xdsl.dialects.builtin import (
     Float64Type,
     IntegerType,
     ModuleOp,
-    SignednessAttr,
     UnrealizedConversionCastOp,
 )
-from xdsl.ir import Attribute, Operation
-from xdsl.irdl import isa
-from xdsl.parser import Signedness
+from xdsl.ir import Attribute
 from xdsl.passes import ModulePass
 from xdsl.pattern_rewriter import (
     GreedyRewritePatternApplier,
@@ -27,7 +24,7 @@ from xdsl.pattern_rewriter import (
     op_type_rewrite_pattern,
 )
 
-from snaxc.dialects import hardfloat
+from snaxc.dialects.hardfloat import AddRecFnOp, FnToRecFnOp, InToRecFnOp, MulRecFnOp, RecFnToFnOp, RecFnToInOp
 
 _type_mapping: dict[type[Attribute], tuple[int, int]] = {
     Float64Type: (11, 53),
@@ -36,35 +33,64 @@ _type_mapping: dict[type[Attribute], tuple[int, int]] = {
     BFloat16Type: (8, 8),
 }
 
-# Op conversion
-_arith_to_hardfloat: dict[type[Operation], type[hardfloat.HardfloatOperation]] = {
-    arith.AddfOp: hardfloat.AddRecFnOp,
-    arith.MulfOp: hardfloat.MulRecFnOp,
-}
 
-
-class ConvertFloatBinaryOps(RewritePattern):
+class ConvertAddSubOp(RewritePattern):
     @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: arith.FloatingPointLikeBinaryOperation, rewriter: PatternRewriter):
+    def match_and_rewrite(self, op: arith.AddfOp | arith.SubfOp, rewriter: PatternRewriter):
         # Get sig_width and exp_witdh:
-        in_type = op.lhs.type
+        in_type = cast(AnyFloat, op.lhs.type)  # These are verified by IRDL
         if type(in_type) not in _type_mapping:
             return
         exp_width, sig_width = _type_mapping[type(in_type)]
-        assert isa(in_type, AnyFloat)
         bitwidth = in_type.bitwidth
-        if type(op) not in _arith_to_hardfloat:
+        match op:
+            case arith.AddfOp():
+                subOp = hw.ConstantOp(0, 1)
+            case arith.SubfOp():
+                subOp = hw.ConstantOp(1, 1)
+        # Create the recode - core_op - unrecode sandwich
+        new_ops = [
+            subOp,
+            cast_lhs := UnrealizedConversionCastOp.get([op.lhs], [IntegerType(bitwidth)]),
+            cast_rhs := UnrealizedConversionCastOp.get([op.rhs], [IntegerType(bitwidth)]),
+            recode_lhs := FnToRecFnOp([cast_lhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
+            recode_rhs := FnToRecFnOp([cast_rhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
+            rounding_mode := hw.ConstantOp(0, 3),
+            tininess := hw.ConstantOp(1, 1),
+            add := AddRecFnOp(
+                [subOp, recode_lhs, recode_rhs, rounding_mode, tininess],
+                [IntegerType(bitwidth + 1)],
+                sig_width,
+                exp_width,
+            ),
+            unrecode := RecFnToFnOp([add], [IntegerType(bitwidth)], sig_width, exp_width),
+            cast_res := UnrealizedConversionCastOp.get([unrecode], [in_type]),
+        ]
+        rewriter.replace_op(op, new_ops=new_ops, new_results=[cast_res.results[0]])
+
+
+class ConvertMulOp(RewritePattern):
+    @op_type_rewrite_pattern
+    def match_and_rewrite(self, op: arith.MulfOp, rewriter: PatternRewriter):
+        # Get sig_width and exp_witdh:
+        in_type = cast(AnyFloat, op.lhs.type)  # These are verified by IRDL
+        if type(in_type) not in _type_mapping:
             return
-        CoreOp = _arith_to_hardfloat[type(op)]
+        exp_width, sig_width = _type_mapping[type(in_type)]
+        bitwidth = in_type.bitwidth
 
         # Create the recode - core_op - unrecode sandwich
         new_ops = [
             cast_lhs := UnrealizedConversionCastOp.get([op.lhs], [IntegerType(bitwidth)]),
             cast_rhs := UnrealizedConversionCastOp.get([op.rhs], [IntegerType(bitwidth)]),
-            recode_lhs := hardfloat.FnToRecFnOp([cast_lhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
-            recode_rhs := hardfloat.FnToRecFnOp([cast_rhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
-            core_op := CoreOp([recode_lhs, recode_rhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
-            unrecode := hardfloat.RecFnToFnOp([core_op], [IntegerType(bitwidth)], sig_width, exp_width),
+            recode_lhs := FnToRecFnOp([cast_lhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
+            recode_rhs := FnToRecFnOp([cast_rhs], [IntegerType(bitwidth + 1)], sig_width, exp_width),
+            rounding_mode := hw.ConstantOp(0, 3),
+            tininess := hw.ConstantOp(1, 1),
+            mul := MulRecFnOp(
+                [recode_lhs, recode_rhs, rounding_mode, tininess], [IntegerType(bitwidth + 1)], sig_width, exp_width
+            ),
+            unrecode := RecFnToFnOp([mul], [IntegerType(bitwidth)], sig_width, exp_width),
             cast_res := UnrealizedConversionCastOp.get([unrecode], [in_type]),
         ]
         rewriter.replace_op(op, new_ops=new_ops, new_results=[cast_res.results[0]])
@@ -72,21 +98,20 @@ class ConvertFloatBinaryOps(RewritePattern):
 
 class ConvertIToFPOp(RewritePattern):
     @op_type_rewrite_pattern
-    def match_and_rewrite(self, op: arith.IntegerToFloatingPointBaseOp, rewriter: PatternRewriter):
-        match op.name:
-            case arith.SIToFPOp.name:
-                props = {"signedness": cast(Attribute, SignednessAttr.get(Signedness.SIGNED))}
-            case arith.UIToFPOp.name:
-                props = {"signedness": cast(Attribute, SignednessAttr.get(Signedness.UNSIGNED))}
-            case _:
-                raise NotImplementedError()
+    def match_and_rewrite(self, op: arith.SIToFPOp | arith.UIToFPOp, rewriter: PatternRewriter):
+        match op:
+            case arith.SIToFPOp():
+                signed_in = hw.ConstantOp(1, 1)
+            case arith.UIToFPOp():
+                signed_in = hw.ConstantOp(0, 1)
         exp_width, sig_width = _type_mapping[type(op.result.type)]
         bitwidth = cast(IntegerType, op.input.type).bitwidth
         new_ops = [
-            rec_fn := hardfloat.InToRecFnOp(
-                [op.input], [IntegerType(bitwidth + 1)], sig_width, exp_width, bitwidth, prop_dict=props
+            signed_in,
+            rec_fn := InToRecFnOp(
+                [signed_in.result, op.input], [IntegerType(bitwidth + 1)], sig_width, exp_width, bitwidth
             ),
-            unrecode := hardfloat.RecFnToFnOp([rec_fn], [IntegerType(bitwidth)], sig_width, exp_width),
+            unrecode := RecFnToFnOp([rec_fn], [IntegerType(bitwidth)], sig_width, exp_width),
             cast_res := UnrealizedConversionCastOp.get([unrecode], [op.result.type]),
         ]
         rewriter.replace_op(op, new_ops=new_ops, new_results=[cast_res.results[0]])
@@ -97,19 +122,18 @@ class ConvertFPToIOp(RewritePattern):
     def match_and_rewrite(self, op: arith.FloatingPointToIntegerBaseOp, rewriter: PatternRewriter):
         match op.name:
             case arith.FPToSIOp.name:
-                props = {"signedness": cast(Attribute, SignednessAttr.get(Signedness.SIGNED))}
+                constant = hw.ConstantOp(1, 1)
             case arith.FPToUIOp.name:
-                props = {"signedness": cast(Attribute, SignednessAttr.get(Signedness.UNSIGNED))}
+                constant = hw.ConstantOp(0, 1)
             case _:
                 raise NotImplementedError()
         exp_width, sig_width = _type_mapping[type(op.input.type)]
         bitwidth = cast(IntegerType, op.input.type).bitwidth
         new_ops = [
+            constant,
             cast_res := UnrealizedConversionCastOp.get([op.input], [op.result.type]),
-            recode := hardfloat.FnToRecFnOp([cast_res], [IntegerType(bitwidth + 1)], sig_width, exp_width),
-            rec_fn := hardfloat.RecFnToInOp(
-                [recode], [IntegerType(bitwidth)], sig_width, exp_width, bitwidth, prop_dict=props
-            ),
+            recode := FnToRecFnOp([cast_res], [IntegerType(bitwidth + 1)], sig_width, exp_width),
+            rec_fn := RecFnToInOp([recode, constant], [IntegerType(bitwidth)], sig_width, exp_width, bitwidth),
         ]
         rewriter.replace_op(op, new_ops=new_ops, new_results=[rec_fn.results[0]])
 
@@ -119,6 +143,6 @@ class ConvertFloatToHardfloatPass(ModulePass):
 
     def apply(self, ctx: Context, op: ModuleOp) -> None:
         PatternRewriteWalker(
-            GreedyRewritePatternApplier([ConvertFloatBinaryOps(), ConvertIToFPOp(), ConvertFPToIOp()]),
+            GreedyRewritePatternApplier([ConvertAddSubOp(), ConvertMulOp(), ConvertIToFPOp(), ConvertFPToIOp()]),
             apply_recursively=False,
         ).rewrite_module(op)
